e7d9df2fb815ae6b7c04f2ae3026084706dade69
Yufang Wang <ywang@openclinica.com>
2010-10-04 19:48:37 +0000

    removed useless messages

--- core/src/main/java/org/akaza/openclinica/dao/core/EntityDAO.java
+++ core/src/main/java/org/akaza/openclinica/dao/core/EntityDAO.java
@@ -7,6 +7,16 @@
  */
 package org.akaza.openclinica.dao.core;
 
+import org.akaza.openclinica.bean.core.ApplicationConstants;
+import org.akaza.openclinica.bean.core.EntityBean;
+import org.akaza.openclinica.bean.core.Status;
+import org.akaza.openclinica.bean.core.Utils;
+import org.akaza.openclinica.bean.extract.ExtractBean;
+import org.akaza.openclinica.bean.managestudy.StudySubjectBean;
+import org.akaza.openclinica.i18n.util.ResourceBundleProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -21,16 +31,6 @@ import java.util.Iterator;
 import java.util.Locale;
 
 import javax.sql.DataSource;
-
-import org.akaza.openclinica.bean.core.ApplicationConstants;
-import org.akaza.openclinica.bean.core.EntityBean;
-import org.akaza.openclinica.bean.core.Status;
-import org.akaza.openclinica.bean.core.Utils;
-import org.akaza.openclinica.bean.extract.ExtractBean;
-import org.akaza.openclinica.bean.managestudy.StudySubjectBean;
-import org.akaza.openclinica.i18n.util.ResourceBundleProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * <p/>
@@ -185,7 +185,6 @@ public abstract class EntityDAO implements DAOInterface {
             rs = ps.executeQuery();
             if (logger.isInfoEnabled()) {
                 logger.info("Executing dynamic query, EntityDAO.select:query " + query);
-                System.out.println("===query="+query);
             }
             signalSuccess();
             results = this.processResultRows(rs);
