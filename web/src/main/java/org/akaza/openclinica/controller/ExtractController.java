00a5b1d09efb8be3d51b2d4b938e9b79c9a16b6c
Jamuna Nyayapathi <jnyayapathi@openclinica.com>
2010-10-04 21:05:00 +0000

    Fixing the xsl location to be abs path, and also to take variables such as $filePath and &datetime.

--- web/src/main/java/org/akaza/openclinica/controller/ExtractController.java
+++ web/src/main/java/org/akaza/openclinica/controller/ExtractController.java
@@ -95,6 +95,11 @@ public class ExtractController {
         System.out.println("found odm xml file path " + generalFileDir);
         // next, can already run jobs, translations, and then add a message to be notified later
         String xsltPath = SQLInitServlet.getField("filePath") + "xslt" + File.separator + epBean.getFileName();
+        String endFilePath = epBean.getFileLocation();
+        if(endFilePath.contains("$filePath"))
+        endFilePath = 	endFilePath.replace("$filePath\\", SQLInitServlet.getField("filePath")+File.separator);
+        if(endFilePath.contains("&datetime"))
+        	endFilePath = endFilePath.replace("&datetime",  sdfDir.format(new java.util.Date()));
         // also need to add the status fields discussed w/ cc:
         // result code, user message, optional URL, archive message, log file message
         // asdf table: sort most recent at top
@@ -102,7 +107,7 @@ public class ExtractController {
         // String xmlFilePath = generalFileDir + ODMXMLFileName;
         SimpleTrigger simpleTrigger = xsltService.generateXsltTrigger(xsltPath, 
                 generalFileDir, // xml_file_path
-                generalFileDir + epBean.getFileLocation() + File.separator, 
+                endFilePath, 
                 resolveExportFilePath(epBean), 
                 dsBean.getId(), 
                 epBean, userBean);
