9af54e9bbe6d60fbac3c543f40e540a91403f966
sshamim <devnull@localhost>
2010-12-20 13:57:30 +0000

    Auto generate study subject id message added to add new subject express page.

--- web/src/main/java/org/akaza/openclinica/control/submit/ListStudySubjectsServlet.java
+++ web/src/main/java/org/akaza/openclinica/control/submit/ListStudySubjectsServlet.java
@@ -88,6 +88,7 @@ public class ListStudySubjectsServlet extends SecureController {
             // int nextLabel = getStudySubjectDAO().findTheGreatestLabel() + 1;
             // request.setAttribute("label", new Integer(nextLabel).toString());
             request.setAttribute("label", resword.getString("id_generated_Save_Add"));
+            fp.addPresetValue("label", resword.getString("id_generated_Save_Add"));
         }
 
         if (fp.getRequest().getParameter("subjectOverlay") == null){
