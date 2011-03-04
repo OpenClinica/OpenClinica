4150bfacbbffb5febecd75a624442661ab629e90
Yufang Wang <ywang@openclinica.com>
2011-03-04 21:15:07 +0000

    6567 fix, no special character allowed in measurement_unit_oid

--- core/src/main/java/org/akaza/openclinica/bean/oid/MeasurementUnitOidGenerator.java
+++ core/src/main/java/org/akaza/openclinica/bean/oid/MeasurementUnitOidGenerator.java
@@ -8,9 +8,7 @@
 package org.akaza.openclinica.bean.oid;
 
 /**
- *
- * @author ywang (May, 2009)
- *
+ * Maximum length of measurement_unit_oid is 40.
  */
 public class MeasurementUnitOidGenerator extends OidGenerator {
 
@@ -25,13 +23,19 @@ public class MeasurementUnitOidGenerator extends OidGenerator {
 
     @Override
     String createOid(String... keys) {
-        String oid = this.truncateToXChars("MU_" + capitalize(stripNonAlphaNumeric(keys[0])),40);
+        String oid = this.truncateToXChars("MU_" + capitalize(stripNonAlphaNumeric(keys[0])),35);
+
+        // If oid is made up of all special characters then
+        if (oid.equals("MU_")) {
+            oid = randomizeOid("MU_");
+        }
+        logger.info("OID : " + oid);
         return oid;
     }
 
     @Override
     String stripNonAlphaNumeric(String input) {
-        return input.trim().replaceAll("\\s+", "");
+        return input.trim().replaceAll("[^a-zA-Z_0-9]", "");
     }
 
     public String generateOidNoValidation(String... keys) throws Exception {
