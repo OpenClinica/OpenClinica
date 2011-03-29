a5bcc45d22df0c2e7b03cef21a820bb7e08120dc
Yufang Wang <ywang@openclinica.com>
2011-03-29 15:53:31 +0000

    fix for dynamics of repeating group

--- core/src/main/java/org/akaza/openclinica/dao/hibernate/DynamicsItemFormMetadataDao.java
+++ core/src/main/java/org/akaza/openclinica/dao/hibernate/DynamicsItemFormMetadataDao.java
@@ -6,7 +6,7 @@ import org.akaza.openclinica.bean.submit.ItemFormMetadataBean;
 import org.akaza.openclinica.domain.crfdata.DynamicsItemFormMetadataBean;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
 
 public class DynamicsItemFormMetadataDao extends AbstractDomainDao<DynamicsItemFormMetadataBean> {
 
@@ -49,5 +49,104 @@ public class DynamicsItemFormMetadataDao extends AbstractDomainDao<DynamicsItemF
         return domainObject;
     }
     
+    @SuppressWarnings("unchecked")
+    public List<Integer> findItemIdsForAGroupInSection(int groupId, int sectionId, int crfVersionId, int eventCrfId) {
+        String query = "select distinct ditem.item_id from dyn_item_form_metadata ditem" 
+            + " where ditem.item_data_id in (select idata.item_data_id from item_data idata"
+            + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
+            + " select distinct igm.item_id from item_group_metadata igm"
+            + " where igm.item_group_id = :groupId and igm.crf_version_id = :crfVersionId and igm.item_id in ("
+            + " select ifm.item_id from item_form_metadata ifm where ifm.show_item='false' and ifm.section_id = :sectionId"
+            + " and ifm.crf_version_id = :crfVersionId))"
+            + " and (idata.status_id != 5 and idata.status_id != 7) )";
+        
+        org.hibernate.Query q = this.getCurrentSession().createSQLQuery(query);
+        q.setInteger("eventCrfId", eventCrfId);
+        q.setInteger("groupId", groupId);
+        q.setInteger("crfVersionId", crfVersionId);
+        q.setInteger("sectionId", sectionId);
+        q.setInteger("crfVersionId", crfVersionId);
+        return new ArrayList<Integer>(q.list());
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<Integer> findShowItemIdsForAGroupInSection(int groupId, int sectionId, int crfVersionId, int eventCrfId) {
+        String query = "select distinct ditem.item_id from dyn_item_form_metadata ditem" 
+            + " where ditem.item_data_id in (select idata.item_data_id from item_data idata"
+            + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
+            + " select distinct igm.item_id from item_group_metadata igm"
+            + " where igm.item_group_id = :groupId and igm.crf_version_id = :crfVersionId and igm.item_id in ("
+            + " select ifm.item_id from item_form_metadata ifm where ifm.show_item='false' and ifm.section_id = :sectionId"
+            + " and ifm.crf_version_id = :crfVersionId))"
+            + " and (idata.status_id != 5 and idata.status_id != 7) )"
+            + " and ditem.show_item='true'";
+        
+        org.hibernate.Query q = this.getCurrentSession().createSQLQuery(query);
+        q.setInteger("eventCrfId", eventCrfId);
+        q.setInteger("groupId", groupId);
+        q.setInteger("crfVersionId", crfVersionId);
+        q.setInteger("sectionId", sectionId);
+        q.setInteger("crfVersionId", crfVersionId);
+        return new ArrayList<Integer>(q.list());
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<Integer> findShowItemDataIdsForAGroupInSection(int groupId, int sectionId, int crfVersionId, int eventCrfId) {
+        String query = "select ditem.item_data_id from dyn_item_form_metadata ditem" 
+            + " where ditem.item_data_id in (select idata.item_data_id from item_data idata"
+            + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
+            + " select distinct igm.item_id from item_group_metadata igm"
+            + " where igm.item_group_id = :groupId and igm.crf_version_id = :crfVersionId and igm.item_id in ("
+            + " select ifm.item_id from item_form_metadata ifm where ifm.show_item='false' and ifm.section_id = :sectionId"
+            + " and ifm.crf_version_id = :crfVersionId))"
+            + " and (idata.status_id != 5 and idata.status_id != 7) )"
+            + " and ditem.show_item='true'";
+        
+        org.hibernate.Query q = this.getCurrentSession().createSQLQuery(query);
+        q.setInteger("eventCrfId", eventCrfId);
+        q.setInteger("groupId", groupId);
+        q.setInteger("crfVersionId", crfVersionId);
+        q.setInteger("sectionId", sectionId);
+        q.setInteger("crfVersionId", crfVersionId);
+        return new ArrayList<Integer>(q.list());
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<Integer> findHideItemDataIdsForAGroupInSection(int groupId, int sectionId, int crfVersionId, int eventCrfId) {
+        String query = "select ditem.item_data_id from dyn_item_form_metadata ditem" 
+            + " where ditem.item_data_id in (select idata.item_data_id from item_data idata"
+            + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
+            + " select distinct igm.item_id from item_group_metadata igm"
+            + " where igm.item_group_id = :groupId and igm.crf_version_id = :crfVersionId and igm.item_id in ("
+            + " select ifm.item_id from item_form_metadata ifm where ifm.show_item='false' and ifm.section_id = :sectionId"
+            + " and ifm.crf_version_id = :crfVersionId))"
+            + " and (idata.status_id != 5 and idata.status_id != 7) )"
+            + " and ditem.show_item='false'";
+        
+        org.hibernate.Query q = this.getCurrentSession().createSQLQuery(query);
+        q.setInteger("eventCrfId", eventCrfId);
+        q.setInteger("groupId", groupId);
+        q.setInteger("crfVersionId", crfVersionId);
+        q.setInteger("sectionId", sectionId);
+        q.setInteger("crfVersionId", crfVersionId);
+        return new ArrayList<Integer>(q.list());
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<Integer> findShowItemDataIdsInSection(int sectionId, int crfVersionId, int eventCrfId) {
+        String query = "select ditem.item_data_id from dyn_item_form_metadata ditem" 
+            + " where ditem.item_data_id in ( select idata.item_data_id from item_data idata"
+            + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
+            + " select ifm.item_id from item_form_metadata ifm where ifm.show_item='false' and ifm.section_id = :sectionId"
+            + " and ifm.crf_version_id = :crfVersionId)"
+            + " and (idata.status_id != 5 and idata.status_id != 7) )"
+            + " and ditem.show_item='true'";
+        
+        org.hibernate.Query q = this.getCurrentSession().createSQLQuery(query);
+        q.setInteger("eventCrfId", eventCrfId);
+        q.setInteger("sectionId", sectionId);
+        q.setInteger("crfVersionId", crfVersionId);
+        return new ArrayList<Integer>(q.list());
+    }
 
 }
