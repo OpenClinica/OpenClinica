ffcb3705e03f5b8b3be1fa77b073bc0b3edf8de1
Pradnya Gawade <pgawade@openclinica.com>
2011-04-05 14:04:53 +0000

    changes for logging usage statistics through OpenClinica instance - issue #8248

---
+++ core/src/main/java/org/akaza/openclinica/domain/OpenClinicaVersionBean.java
@@ -0,0 +1,79 @@
+/* 
+ * OpenClinica is distributed under the
+ * GNU Lesser General Public License (GNU LGPL).
+ * For details see: http://www.openclinica.org/license
+ *
+ * Copyright 2003-2008 Akaza Research 
+ */
+package org.akaza.openclinica.domain;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+/**
+ * <p>
+ * OpenClinica Version
+ * </p>
+ * 
+ * @author Pradnya Gawade
+ */
+@Entity
+@Table(name = "openclinica_version")
+@GenericGenerator(name = "id-generator", strategy = "native", parameters = { @Parameter(name = "sequence", value = "openclinica_version_id_seq") })
+public class OpenClinicaVersionBean extends AbstractMutableDomainObject {
+
+    private String name;
+    private String build_number;
+    private Timestamp update_timestamp;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the test_path
+     */
+    public String getBuild_number() {
+        return build_number;
+    }
+
+    /**
+     * @param test_path
+     *            the test_path to set
+     */
+    public void setBuild_number(String build_number) {
+        this.build_number = build_number;
+    }
+
+    /**
+     * @return the update_timestamp
+     */
+    public Timestamp getUpdate_timestamp() {
+        return update_timestamp;
+    }
+
+    /**
+     * @param update_timestamp
+     *            the update_timestamp to set
+     */
+    public void setUpdate_timestamp(Timestamp update_timestamp) {
+        this.update_timestamp = update_timestamp;
+    }
+
+}
---
+++ core/src/main/java/org/akaza/openclinica/domain/usageStats/LogUsageStatsBean.java
@@ -0,0 +1,81 @@
+/* 
+ * OpenClinica is distributed under the
+ * GNU Lesser General Public License (GNU LGPL).
+ * For details see: http://www.openclinica.org/license
+ *
+ * Copyright 2003-2008 Akaza Research 
+ */
+package org.akaza.openclinica.domain.usageStats;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.akaza.openclinica.domain.AbstractMutableDomainObject;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+/**
+ * <p>
+ * Log Usage Statistics
+ * </p>
+ * 
+ * @author Pradnya Gawade
+ */
+@Entity
+@Table(name = "usage_statistics_data")
+@GenericGenerator(name = "id-generator", strategy = "native", parameters = { @Parameter(name = "sequence", value = "usage_statistics_data_id_seq") })
+public class LogUsageStatsBean extends AbstractMutableDomainObject {
+
+    private String param_key;
+    private String param_value;
+    private Timestamp update_timestamp;
+
+
+    /**
+     * @return the param_key
+     */
+    public String getParam_key() {
+        return param_key;
+    }
+
+    /**
+     * @param param_key
+     *            the param_key to set
+     */
+    public void setParam_key(String param_key) {
+        this.param_key = param_key;
+    }
+
+    /**
+     * @return the param_value
+     */
+    public String getParam_value() {
+        return param_value;
+    }
+
+    /**
+     * @param param_value
+     *            the param_value to set
+     */
+    public void setParam_value(String param_value) {
+        this.param_value = param_value;
+    }
+
+    /**
+     * @return the update_timestamp
+     */
+    public Timestamp getUpdate_timestamp() {
+        return update_timestamp;
+    }
+
+    /**
+     * @param update_timestamp
+     *            the update_timestamp to set
+     */
+    public void setUpdate_timestamp(Timestamp update_timestamp) {
+        this.update_timestamp = update_timestamp;
+    }
+
+}
