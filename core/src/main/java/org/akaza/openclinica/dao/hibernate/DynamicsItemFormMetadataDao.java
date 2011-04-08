be3ffcf50377f62b15dd93ed1c76bb76d89cf662
Yufang Wang <ywang@openclinica.com>
2011-04-08 13:00:33 +0000

    part of 5894 fix

--- core/src/main/java/org/akaza/openclinica/dao/hibernate/DynamicsItemFormMetadataDao.java
+++ core/src/main/java/org/akaza/openclinica/dao/hibernate/DynamicsItemFormMetadataDao.java
@@ -3,6 +3,7 @@ package org.akaza.openclinica.dao.hibernate;
 import org.akaza.openclinica.bean.submit.EventCRFBean;
 import org.akaza.openclinica.bean.submit.ItemDataBean;
 import org.akaza.openclinica.bean.submit.ItemFormMetadataBean;
+import org.akaza.openclinica.dao.core.CoreResources;
 import org.akaza.openclinica.domain.crfdata.DynamicsItemFormMetadataBean;
 
 import java.util.ArrayList;
@@ -51,7 +52,18 @@ public class DynamicsItemFormMetadataDao extends AbstractDomainDao<DynamicsItemF
     
     @SuppressWarnings("unchecked")
     public List<Integer> findItemIdsForAGroupInSection(int groupId, int sectionId, int crfVersionId, int eventCrfId) {
-        String query = "select distinct ditem.item_id from dyn_item_form_metadata ditem" 
+        String query = "";
+        if ("oracle".equalsIgnoreCase(CoreResources.getDBName())) {
+            query = "select distinct ditem.item_id from dyn_item_form_metadata ditem" 
+                + " where ditem.item_data_id in (select idata.item_data_id from item_data idata"
+                + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
+                + " select distinct igm.item_id from item_group_metadata igm"
+                + " where igm.item_group_id = :groupId and igm.crf_version_id = :crfVersionId and igm.item_id in ("
+                + " select ifm.item_id from item_form_metadata ifm where ifm.show_item=0 and ifm.section_id = :sectionId"
+                + " and ifm.crf_version_id = :crfVersionId))"
+                + " and (idata.status_id != 5 and idata.status_id != 7) )";
+        } else {
+            query = "select distinct ditem.item_id from dyn_item_form_metadata ditem" 
             + " where ditem.item_data_id in (select idata.item_data_id from item_data idata"
             + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
             + " select distinct igm.item_id from item_group_metadata igm"
@@ -59,6 +71,7 @@ public class DynamicsItemFormMetadataDao extends AbstractDomainDao<DynamicsItemF
             + " select ifm.item_id from item_form_metadata ifm where ifm.show_item='false' and ifm.section_id = :sectionId"
             + " and ifm.crf_version_id = :crfVersionId))"
             + " and (idata.status_id != 5 and idata.status_id != 7) )";
+        }
         
         org.hibernate.Query q = this.getCurrentSession().createSQLQuery(query);
         q.setInteger("eventCrfId", eventCrfId);
@@ -71,7 +84,19 @@ public class DynamicsItemFormMetadataDao extends AbstractDomainDao<DynamicsItemF
     
     @SuppressWarnings("unchecked")
     public List<Integer> findShowItemIdsForAGroupInSection(int groupId, int sectionId, int crfVersionId, int eventCrfId) {
-        String query = "select distinct ditem.item_id from dyn_item_form_metadata ditem" 
+        String query = "";
+        if ("oracle".equalsIgnoreCase(CoreResources.getDBName())) {
+            query = "select distinct ditem.item_id from dyn_item_form_metadata ditem" 
+                + " where ditem.item_data_id in (select idata.item_data_id from item_data idata"
+                + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
+                + " select distinct igm.item_id from item_group_metadata igm"
+                + " where igm.item_group_id = :groupId and igm.crf_version_id = :crfVersionId and igm.item_id in ("
+                + " select ifm.item_id from item_form_metadata ifm where ifm.show_item=0 and ifm.section_id = :sectionId"
+                + " and ifm.crf_version_id = :crfVersionId))"
+                + " and (idata.status_id != 5 and idata.status_id != 7) )"
+                + " and ditem.show_item=1";
+        } else {
+        query = "select distinct ditem.item_id from dyn_item_form_metadata ditem" 
             + " where ditem.item_data_id in (select idata.item_data_id from item_data idata"
             + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
             + " select distinct igm.item_id from item_group_metadata igm"
@@ -80,6 +105,7 @@ public class DynamicsItemFormMetadataDao extends AbstractDomainDao<DynamicsItemF
             + " and ifm.crf_version_id = :crfVersionId))"
             + " and (idata.status_id != 5 and idata.status_id != 7) )"
             + " and ditem.show_item='true'";
+        }
         
         org.hibernate.Query q = this.getCurrentSession().createSQLQuery(query);
         q.setInteger("eventCrfId", eventCrfId);
@@ -92,7 +118,19 @@ public class DynamicsItemFormMetadataDao extends AbstractDomainDao<DynamicsItemF
     
     @SuppressWarnings("unchecked")
     public List<Integer> findShowItemDataIdsForAGroupInSection(int groupId, int sectionId, int crfVersionId, int eventCrfId) {
-        String query = "select ditem.item_data_id from dyn_item_form_metadata ditem" 
+        String query = "";
+        if ("oracle".equalsIgnoreCase(CoreResources.getDBName())) {
+            query = "select ditem.item_data_id from dyn_item_form_metadata ditem" 
+                + " where ditem.item_data_id in (select idata.item_data_id from item_data idata"
+                + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
+                + " select distinct igm.item_id from item_group_metadata igm"
+                + " where igm.item_group_id = :groupId and igm.crf_version_id = :crfVersionId and igm.item_id in ("
+                + " select ifm.item_id from item_form_metadata ifm where ifm.show_item=0 and ifm.section_id = :sectionId"
+                + " and ifm.crf_version_id = :crfVersionId))"
+                + " and (idata.status_id != 5 and idata.status_id != 7) )"
+                + " and ditem.show_item=1";
+        } else {
+        query = "select ditem.item_data_id from dyn_item_form_metadata ditem" 
             + " where ditem.item_data_id in (select idata.item_data_id from item_data idata"
             + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
             + " select distinct igm.item_id from item_group_metadata igm"
@@ -101,6 +139,7 @@ public class DynamicsItemFormMetadataDao extends AbstractDomainDao<DynamicsItemF
             + " and ifm.crf_version_id = :crfVersionId))"
             + " and (idata.status_id != 5 and idata.status_id != 7) )"
             + " and ditem.show_item='true'";
+        }
         
         org.hibernate.Query q = this.getCurrentSession().createSQLQuery(query);
         q.setInteger("eventCrfId", eventCrfId);
@@ -113,7 +152,19 @@ public class DynamicsItemFormMetadataDao extends AbstractDomainDao<DynamicsItemF
     
     @SuppressWarnings("unchecked")
     public List<Integer> findHideItemDataIdsForAGroupInSection(int groupId, int sectionId, int crfVersionId, int eventCrfId) {
-        String query = "select ditem.item_data_id from dyn_item_form_metadata ditem" 
+        String query = "";
+        if ("oracle".equalsIgnoreCase(CoreResources.getDBName())) {
+            query = "select ditem.item_data_id from dyn_item_form_metadata ditem" 
+                + " where ditem.item_data_id in (select idata.item_data_id from item_data idata"
+                + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
+                + " select distinct igm.item_id from item_group_metadata igm"
+                + " where igm.item_group_id = :groupId and igm.crf_version_id = :crfVersionId and igm.item_id in ("
+                + " select ifm.item_id from item_form_metadata ifm where ifm.show_item=0 and ifm.section_id = :sectionId"
+                + " and ifm.crf_version_id = :crfVersionId))"
+                + " and (idata.status_id != 5 and idata.status_id != 7) )"
+                + " and ditem.show_item=0";
+        } else {
+        query = "select ditem.item_data_id from dyn_item_form_metadata ditem" 
             + " where ditem.item_data_id in (select idata.item_data_id from item_data idata"
             + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
             + " select distinct igm.item_id from item_group_metadata igm"
@@ -122,6 +173,7 @@ public class DynamicsItemFormMetadataDao extends AbstractDomainDao<DynamicsItemF
             + " and ifm.crf_version_id = :crfVersionId))"
             + " and (idata.status_id != 5 and idata.status_id != 7) )"
             + " and ditem.show_item='false'";
+        }
         
         org.hibernate.Query q = this.getCurrentSession().createSQLQuery(query);
         q.setInteger("eventCrfId", eventCrfId);
@@ -134,19 +186,60 @@ public class DynamicsItemFormMetadataDao extends AbstractDomainDao<DynamicsItemF
     
     @SuppressWarnings("unchecked")
     public List<Integer> findShowItemDataIdsInSection(int sectionId, int crfVersionId, int eventCrfId) {
-        String query = "select ditem.item_data_id from dyn_item_form_metadata ditem" 
+        String query = "";
+        if ("oracle".equalsIgnoreCase(CoreResources.getDBName())) {
+            query = "select ditem.item_data_id from dyn_item_form_metadata ditem" 
+                + " where ditem.item_data_id in ( select idata.item_data_id from item_data idata"
+                + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
+                + " select ifm.item_id from item_form_metadata ifm where ifm.show_item=0 and ifm.section_id = :sectionId"
+                + " and ifm.crf_version_id = :crfVersionId)"
+                + " and (idata.status_id != 5 and idata.status_id != 7) )"
+                + " and ditem.show_item=1";   
+        }else {
+        query = "select ditem.item_data_id from dyn_item_form_metadata ditem" 
             + " where ditem.item_data_id in ( select idata.item_data_id from item_data idata"
             + " where idata.event_crf_id = :eventCrfId and idata.item_id in ("
             + " select ifm.item_id from item_form_metadata ifm where ifm.show_item='false' and ifm.section_id = :sectionId"
             + " and ifm.crf_version_id = :crfVersionId)"
             + " and (idata.status_id != 5 and idata.status_id != 7) )"
             + " and ditem.show_item='true'";
+        }
         
         org.hibernate.Query q = this.getCurrentSession().createSQLQuery(query);
         q.setInteger("eventCrfId", eventCrfId);
         q.setInteger("sectionId", sectionId);
         q.setInteger("crfVersionId", crfVersionId);
         return new ArrayList<Integer>(q.list());
+    }
+    
+    public Boolean hasShowingInSection(int sectionId, int crfVersionId, int eventCrfId) {
+        String query = "";
+        if ("oracle".equalsIgnoreCase(CoreResources.getDBName())) {
+            query = "select di.item_id from dyn_item_form_metadata di where di.item_data_id in ("
+                + " select ida.item_data_id from item_data ida where ida.event_crf_id = :eventCrfId and ida.item_id in"
+                + "       (select ifm.item_id from item_form_metadata ifm where ifm.section_id = :sectionId and ifm.crf_version_id = :crfVersionId"
+                + "          and ifm.item_id not in  (select distinct igm.item_id from item_group_metadata igm where igm.crf_version_id = :crfVersionId" 
+                + "          and igm.show_group = 0 and igm.name != 'Ungrouped'"
+                + "          and igm.item_id in (select im.item_id from item_form_metadata im where im.section_id = :sectionId and im.crf_version_id = :crfVersionId))"
+                + "        )and (ida.status_id != 5 and ida.status_id != 7) ) and di.show_item = 1 and rownum = 1" ;   
+        } else {
+        query = "select di.item_id from dyn_item_form_metadata di where di.item_data_id in ("
+            + " select ida.item_data_id from item_data ida where ida.event_crf_id = :eventCrfId and ida.item_id in"
+            + "       (select ifm.item_id from item_form_metadata ifm where ifm.section_id = :sectionId and ifm.crf_version_id = :crfVersionId"
+            + "          and ifm.item_id not in  (select distinct igm.item_id from item_group_metadata igm where igm.crf_version_id = :crfVersionId" 
+            + "          and igm.show_group = 'false' and igm.name != 'Ungrouped'"
+            + "          and igm.item_id in (select im.item_id from item_form_metadata im where im.section_id = :sectionId and im.crf_version_id = :crfVersionId))"
+            + "        )and (ida.status_id != 5 and ida.status_id != 7) ) and di.show_item = 'true' limit 1" ;
+        }
+        
+        org.hibernate.Query q = this.getCurrentSession().createSQLQuery(query);
+        q.setInteger("eventCrfId", eventCrfId);
+        q.setInteger("sectionId", sectionId);
+        q.setInteger("crfVersionId", crfVersionId);
+        q.setInteger("crfVersionId", crfVersionId);
+        q.setInteger("sectionId", sectionId);
+        q.setInteger("crfVersionId", crfVersionId);
+        return q.list() != null && q.list().size() > 0;
     }
 
 }
--- core/src/main/java/org/akaza/openclinica/dao/hibernate/DynamicsItemGroupMetadataDao.java
+++ core/src/main/java/org/akaza/openclinica/dao/hibernate/DynamicsItemGroupMetadataDao.java
@@ -1,9 +1,8 @@
 package org.akaza.openclinica.dao.hibernate;
 
 import org.akaza.openclinica.bean.submit.EventCRFBean;
-import org.akaza.openclinica.bean.submit.ItemGroupBean;
 import org.akaza.openclinica.bean.submit.ItemGroupMetadataBean;
-import org.akaza.openclinica.domain.crfdata.DynamicsItemFormMetadataBean;
+import org.akaza.openclinica.dao.core.CoreResources;
 import org.akaza.openclinica.domain.crfdata.DynamicsItemGroupMetadataBean;
 
 public class DynamicsItemGroupMetadataDao extends AbstractDomainDao<DynamicsItemGroupMetadataBean>{
@@ -33,5 +32,29 @@ public class DynamicsItemGroupMetadataDao extends AbstractDomainDao<DynamicsItem
         q.setInteger("item_group_id", new Integer(metadataBean.getItemGroupId()));
         q.setInteger("event_crf_id", new Integer(eventCrfBeanId));
         return (DynamicsItemGroupMetadataBean) q.uniqueResult();
+    }
+    
+    public Boolean hasShowingInSection(int sectionId, int crfVersionId, int eventCrfId) {
+        String query = "";
+        if ("oracle".equalsIgnoreCase(CoreResources.getDBName())) {
+            query = "select dg.item_group_id from dyn_item_group_metadata dg where dg.event_crf_id = :eventCrfId and dg.item_group_metadata_id in ("
+                + " select distinct igm.item_group_metadata_id from item_group_metadata igm where igm.crf_version_id = :crfVersionId"
+                + " and igm.show_group = 0"
+                + " and igm.item_id in (select im.item_id from item_form_metadata im where im.section_id = :sectionId and im.crf_version_id = :crfVersionId))"
+                + " and dg.show_group = 1 and rownum = 1";
+        } else {
+        query = "select dg.item_group_id from dyn_item_group_metadata dg where dg.event_crf_id = :eventCrfId and dg.item_group_metadata_id in ("
+                + " select distinct igm.item_group_metadata_id from item_group_metadata igm where igm.crf_version_id = :crfVersionId"
+                + " and igm.show_group = 'false'"
+                + " and igm.item_id in (select im.item_id from item_form_metadata im where im.section_id = :sectionId and im.crf_version_id = :crfVersionId))"
+                + " and dg.show_group = 'true' limit 1";
+        }
+        
+        org.hibernate.Query q = this.getCurrentSession().createSQLQuery(query);
+        q.setInteger("eventCrfId", eventCrfId);
+        q.setInteger("crfVersionId", crfVersionId);
+        q.setInteger("sectionId", sectionId);
+        q.setInteger("crfVersionId", crfVersionId);
+        return q.list() != null && q.list().size() > 0;
     }
 }
