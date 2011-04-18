32bca365b6c9d2bdfc09b051a0be4e8ce2f21b8d
Yufang Wang <ywang@openclinica.com>
2011-04-18 21:42:29 +0000

    modified showItem condition

--- web/src/main/java/org/akaza/openclinica/control/submit/DoubleDataEntryServlet.java
+++ web/src/main/java/org/akaza/openclinica/control/submit/DoubleDataEntryServlet.java
@@ -228,7 +228,7 @@ public class DoubleDataEntryServlet extends DataEntryServlet {
         }
         boolean showOriginalItem = getItemMetadataService().isShown(dib.getItem().getId(), ecb, valueToCompare);// was dib.getData()
         boolean showItem = dib.getMetadata().isShowItem();
-        if(dib.getScdData().getScdItemMetadataBean().getScdItemFormMetadataId()>0) {
+        if(!showItem && dib.getScdData().getScdItemMetadataBean().getScdItemFormMetadataId()>0) {
             showItem = true;
         }
         boolean showDuplicateItem = getItemMetadataService().hasPassedDDE(dib.getMetadata(), ecb, valueToCompare);//.isShown(dib.getItem().getId(), ecb, dib.getDbData());// where is the set db data? 
