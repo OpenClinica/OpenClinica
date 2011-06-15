8d095ffcf61d15e9305c77dd47fef9242a1aaa9d
Jamuna Nyayapathi <jnyayapathi@openclinica.com>
2011-06-15 14:41:51 +0000

    testing unit tests

--- core/src/test/java/org/akaza/openclinica/dao/rule/RuleSetRuleDaoTest.java
+++ core/src/test/java/org/akaza/openclinica/dao/rule/RuleSetRuleDaoTest.java
@@ -12,43 +12,40 @@ import java.util.List;
 
 public class RuleSetRuleDaoTest extends HibernateOcDbTestCase {
 
-    public RuleSetRuleDaoTest() {
-      //  super();
-    }
+ /*   public RuleSetRuleDaoTest() {
+        super();
+    }*/
 
     public void testFindById() {
-//        RuleSetRuleDao ruleSetRuleDao = (RuleSetRuleDao) getContext().getBean("ruleSetRuleDao");
-//        RuleSetRuleBean ruleSetRuleBean = null;
-//        ruleSetRuleBean = ruleSetRuleDao.findById(3);
-//
-//        // Test RuleSetRule
-//        assertNotNull("RuleSet is null", ruleSetRuleBean);
-//        assertEquals("The id of the retrieved RuleSet should be 1", new Integer(3), ruleSetRuleBean.getId());
-        assertTrue(true);
+        RuleSetRuleDao ruleSetRuleDao = (RuleSetRuleDao) getContext().getBean("ruleSetRuleDao");
+        RuleSetRuleBean ruleSetRuleBean = null;
+        ruleSetRuleBean = ruleSetRuleDao.findById(3);
+
+        // Test RuleSetRule
+        assertNotNull("RuleSet is null", ruleSetRuleBean);
+        assertEquals("The id of the retrieved RuleSet should be 1", new Integer(3), ruleSetRuleBean.getId());
 
     }
 
     public void testFindByIdEmptyResultSet() {
-//        RuleSetRuleDao ruleSetRuleDao = (RuleSetRuleDao) getContext().getBean("ruleSetRuleDao");
-//
-//        RuleSetRuleBean ruleSetRuleBean = null;
-//        ruleSetRuleBean = ruleSetRuleDao.findById(6);
-//
-//        // Test Rule
-//        assertNull("RuleSet is null", ruleSetRuleBean);
-        assertTrue(true);
+        RuleSetRuleDao ruleSetRuleDao = (RuleSetRuleDao) getContext().getBean("ruleSetRuleDao");
+
+        RuleSetRuleBean ruleSetRuleBean = null;
+        ruleSetRuleBean = ruleSetRuleDao.findById(6);
+
+        // Test Rule
+        assertNull("RuleSet is null", ruleSetRuleBean);
     }
 
     public void testFindByRuleSetBeanAndRuleBean() {
-//        RuleDao ruleDao = (RuleDao) getContext().getBean("ruleDao");
-//        RuleSetDao ruleSetDao = (RuleSetDao) getContext().getBean("ruleSetDao");
-//        RuleSetRuleDao ruleSetRuleDao = (RuleSetRuleDao) getContext().getBean("ruleSetRuleDao");
-//        RuleBean persistentRuleBean = ruleDao.findById(-1);
-//        RuleSetBean persistentRuleSetBean = ruleSetDao.findById(-1);
-//        List<RuleSetRuleBean> ruleSetRules = ruleSetRuleDao.findByRuleSetBeanAndRuleBean(persistentRuleSetBean, persistentRuleBean);
-//
-//        assertNotNull("RuleSetRules is null", ruleSetRules);
-//        assertEquals("The size of RuleSetRules should be 1", new Integer(1), new Integer(ruleSetRules.size()));
-        assertTrue(true);
+        RuleDao ruleDao = (RuleDao) getContext().getBean("ruleDao");
+        RuleSetDao ruleSetDao = (RuleSetDao) getContext().getBean("ruleSetDao");
+        RuleSetRuleDao ruleSetRuleDao = (RuleSetRuleDao) getContext().getBean("ruleSetRuleDao");
+        RuleBean persistentRuleBean = ruleDao.findById(-1);
+        RuleSetBean persistentRuleSetBean = ruleSetDao.findById(-1);
+        List<RuleSetRuleBean> ruleSetRules = ruleSetRuleDao.findByRuleSetBeanAndRuleBean(persistentRuleSetBean, persistentRuleBean);
+
+        assertNotNull("RuleSetRules is null", ruleSetRules);
+        assertEquals("The size of RuleSetRules should be 1", new Integer(1), new Integer(ruleSetRules.size()));
     }
 }
