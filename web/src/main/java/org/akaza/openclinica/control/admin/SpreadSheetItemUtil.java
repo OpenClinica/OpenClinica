bbdde764567183587efdcdd1e74c168e423d9009
Helen Taycher <htaycher@openclinica.com>
2012-05-15 09:48:54 -0400

    13818 : CRF upload: allows default value for file 14142 : CRF Upload: default value for single-select can be more than one value 13634 : Data not visible in 2nd pass of data entry where item has parent_item 13917 : CRF upload: need to check for a min GROUP_REPEAT_NUMBER for a group 13923 : Repeating Groups: CRF can be uploaded if group spread over two section
    The following changes have been done as part of code fix without reported issue:
    
    a) html_error hash key should be 'spreadSheetNumber+',' + row_number + ","+columnNumber -> in many cases empty space have been inserted before columnNumber;
    b) error message use of property 'items_worksheet'+"." replaced by 'items_worksheet_with_dot' ;
    c) printStackTrace -> logger.error( e.getMessage())
    d) fixes to page_messages (broken by CCHMC merge)
    e) subheader -> htmlerror added;
    f) units - max length check added

--- web/src/main/java/org/akaza/openclinica/control/admin/SpreadSheetItemUtil.java
+++ web/src/main/java/org/akaza/openclinica/control/admin/SpreadSheetItemUtil.java
@@ -1,10 +1,28 @@
+/*
+ * OpenClinica is distributed under the
+ * GNU Lesser General Public License (GNU LGPL).
+
+ * For details see: http://www.openclinica.org/license
+ * copyright 2003-2011 Akaza Research
+ */
+
 package org.akaza.openclinica.control.admin;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
+import org.akaza.openclinica.bean.core.ApplicationConstants;
+import org.akaza.openclinica.bean.core.ResponseType;
+import org.akaza.openclinica.bean.core.Utils;
+import org.akaza.openclinica.bean.managestudy.DiscrepancyNoteBean;
+import org.akaza.openclinica.bean.submit.ItemGroupBean;
 import org.akaza.openclinica.core.form.StringUtil;
-import org.akaza.openclinica.core.util.CrfTemplateColumnName;
+import org.akaza.openclinica.core.util.CrfTemplateColumnNameEnum;
 
 public class SpreadSheetItemUtil {
 	
@@ -25,9 +43,10 @@ public class SpreadSheetItemUtil {
 	private String column_number;//11
 	private String page_number;//12
 	private String question_number;//13
-	private String response_type;//14
+	private int response_type_id;//14
 	private String response_label;//15
 	private String response_options_text;//16
+	private String[] response_options;
 	private String response_values_or_calculations;//17
 	private String response_layout;//18
 	private String default_value;//19
@@ -44,6 +63,7 @@ public class SpreadSheetItemUtil {
 	public SpreadSheetItemUtil(){}
 	
 	private String cleanProperty(String property){
+		property= property.trim();
 		return property.replaceAll("<[^>]*>", "");
         
 	}
@@ -64,6 +84,7 @@ public class SpreadSheetItemUtil {
 	 * @param item_name the item_name to set
 	 */
 	public void setItemName(String item_name) {
+		item_name = cleanProperty(item_name);
 		this.item_name = item_name;
 	}
 	/**
@@ -117,26 +138,29 @@ public class SpreadSheetItemUtil {
 	/**
 	 * @return the section_label
 	 */
-	public String getSection_label() {
+	public String getSectionLabel() {
 		return section_label;
 	}
 	/**
 	 * @param section_label the section_label to set
 	 */
-	public void setSection_label(String section_label) {
-		this.section_label = section_label;
+	public void setSectionLabel(String section_label) {
+		this.section_label = cleanProperty(section_label);
+       
+       
+		
 	}
 	/**
 	 * @return the group_label
 	 */
-	public String getGroup_label() {
+	public String getGroupLabel() {
 		return group_label;
 	}
 	/**
 	 * @param group_label the group_label to set
 	 */
-	public void setGroup_label(String group_label) {
-		this.group_label = group_label;
+	public void setGroupLabel(String group_label) {
+		this.group_label =  cleanProperty(group_label);
 	}
 	/**
 	 * @return the header
@@ -214,27 +238,17 @@ public class SpreadSheetItemUtil {
 	/**
 	 * @return the responSe_type
 	 */
-	public String getResponSe_type() {
-		return response_type;
+	public int getResponseTypeId() {
+		return response_type_id;
 	}
 	/**
 	 * @param responSe_type the responSe_type to set
 	 */
-	public void setResponSe_type(String responSe_type) {
-		this.response_type = responSe_type;
+	public void setResponseTypeId(int response_type_id) {
+		this.response_type_id = response_type_id;
 	}
-	/**
-	 * @return the response_type
-	 */
-	public String getResponse_type() {
-		return response_type;
-	}
-	/**
-	 * @param response_type the response_type to set
-	 */
-	public void setResponse_type(String response_type) {
-		this.response_type = response_type;
-	}
+	
+	
 	/**
 	 * @return the response_label
 	 */
@@ -287,26 +301,26 @@ public class SpreadSheetItemUtil {
 	/**
 	 * @return the default_value
 	 */
-	public String getDefault_value() {
+	public String getDefaultValue() {
 		return default_value;
 	}
 	/**
 	 * @param default_value the default_value to set
 	 */
-	public void setDefault_value(String default_value) {
-		this.default_value = default_value;
+	public void setDefaultValue(String default_value) {
+		this.default_value = cleanProperty(default_value);
 	}
 	/**
 	 * @return the data_type
 	 */
-	public String getData_type() {
+	public String getDataType() {
 		return data_type;
 	}
 	/**
 	 * @param data_type the data_type to set
 	 */
-	public void setData_type(String data_type) {
-		this.data_type = data_type;
+	public void setDataType(String data_type) {
+		this.data_type = cleanProperty(data_type);
 	}
 	/**
 	 * @return the width_decimal
@@ -386,10 +400,10 @@ public class SpreadSheetItemUtil {
 	 */
 	public static boolean isItemWithSameParameterExists(
 			String column_value, ArrayList< SpreadSheetItemUtil> row_items){
-		SpreadSheetItemUtil item =  isItemWithSameParameterExists(CrfTemplateColumnName.ITEM_NAME, column_value, row_items);
+		SpreadSheetItemUtil item =  isItemWithSameParameterExists(CrfTemplateColumnNameEnum.ITEM_NAME, column_value, row_items);
 		return (item==null);
 	}
-	public static SpreadSheetItemUtil isItemWithSameParameterExists(CrfTemplateColumnName param_column_index, 
+	public static SpreadSheetItemUtil isItemWithSameParameterExists(CrfTemplateColumnNameEnum param_column_index, 
 			String column_value, ArrayList< SpreadSheetItemUtil> row_items){
 		for (SpreadSheetItemUtil cur_item : row_items ){
 			switch (param_column_index){
@@ -405,33 +419,232 @@ public class SpreadSheetItemUtil {
 		return null;
 		
 	}
-	
+	//TODO if we ever go to normal OO parsing of spreadsheet this method should be moved to 
+	//SpredSheetGroupUtil
+	//the problem here that Group now can be ungrouped group
+	public static void versifySectionGroupPlacementForItems(ArrayList< SpreadSheetItemUtil> row_items,
+			ArrayList<String> ver_errors, HashMap<String,String> htmlErrors,
+			int sheetNumber,
+			ResourceBundle resPageMsg,
+			HashMap<String, ItemGroupBean> itemGroups){
+		HashMap <String,String> group_section_map = new HashMap <String,String>();
+		String section_label;
+		int row_number=1;
+		for (SpreadSheetItemUtil cur_item : row_items){
+			row_number++;
+			if ( cur_item.getGroupLabel() == null || cur_item.getGroupLabel().length()<1 ){
+				continue;
+			}
+			//verify that this is repeating group
+			ItemGroupBean item_group = itemGroups.get(cur_item.getGroupLabel());
+			boolean isRepeatingGroup=item_group.getMeta().isRepeatingGroup();
+			if (!isRepeatingGroup){
+				continue;
+			}
+			section_label = group_section_map.get(cur_item.getGroupLabel());
+			if (section_label != null){//not first item in group
+				if (! section_label.equals(cur_item.getSectionLabel())){//error: items of one group belong to more than one section
+					ver_errors.add(resPageMsg.getString("group_in_several_sections") + cur_item.getGroupLabel()+"'.");
+		            htmlErrors.put(sheetNumber + "," + (row_number-1) + ","+CrfTemplateColumnNameEnum.GROUP_LABEL.getCellNumber(), 
+		            		resPageMsg.getString("INVALID_VALUE") );
+		  
+				}
+			}else{//first item in group
+				group_section_map.put(cur_item.getGroupLabel(), cur_item.getSectionLabel());
+			}
+			
+			
+		}
+	}
 	////////////////////////////////////// verification rules
-	@SuppressWarnings("deprecation")
-	public ArrayList<String> verifyParentID(ArrayList< SpreadSheetItemUtil> row_items, ResourceBundle resPageMsg){
-		ArrayList<String> ver_errors= new ArrayList<String>();
+	public void verifyParentID(ArrayList< SpreadSheetItemUtil> row_items, ArrayList<String> ver_errors, 
+			HashMap<String,String> htmlErrors,  int sheetNumber, ResourceBundle resPageMsg,
+			HashMap<String, ItemGroupBean> itemGroups){
 		
-        // BWP>>Prevent parent names that equal the Item names
+		int row_number = row_items.size();
+        // BWP>>Prevent parent names that equal the Item names 
         if (this.getItemName() != null && this.getItemName().equalsIgnoreCase(this.getParentItem())) {
         	this.setParentItem( "");
-        	return ver_errors;
+        	
         }
         
         if(!StringUtil.isBlank(this.getParentItem())){
-        	SpreadSheetItemUtil cur_item = SpreadSheetItemUtil.isItemWithSameParameterExists(CrfTemplateColumnName.ITEM_NAME, 
+        	SpreadSheetItemUtil cur_item = SpreadSheetItemUtil.isItemWithSameParameterExists(CrfTemplateColumnNameEnum.ITEM_NAME, 
         			this.getParentItem(),  row_items);
         	// Checking for a valid parent item name
             if(cur_item == null){
-            	ver_errors.add(resPageMsg.getString("parent_id")+row_items.size()+resPageMsg.getString("parent_id_1"));
+            	ver_errors.add(resPageMsg.getString("parent_id")+row_number+resPageMsg.getString("parent_id_1"));
+            	 htmlErrors.put(sheetNumber + "," + row_number + ","+CrfTemplateColumnNameEnum.PARENT_ITEM.getCellNumber(), resPageMsg.getString("INVALID_FIELD"));
+            	 
             }
-            //prevent more than one level of hierarchy for parent names 
-            if ( cur_item.getParentItem() != null){
+            //prevent more than one level of hierarchy for parent names (new ver)
+            if ( cur_item != null && cur_item.getParentItem() != null && cur_item.getParentItem().length()>0){
             	ver_errors.add(resPageMsg.getString("nested_parent_id")+row_items.size()+resPageMsg.getString("nested_parent_id_1"));
-            	   
+            	 htmlErrors.put(sheetNumber + "," + row_number + ","+CrfTemplateColumnNameEnum.PARENT_ITEM.getCellNumber(), resPageMsg.getString("INVALID_FIELD"));   
+            }
+            //prevent item in RGroup to have parent id (new ver)
+          //verify that this is repeating group
+            if ( itemGroups != null && itemGroups.size() > 0){
+				ItemGroupBean item_group = itemGroups.get(this.getGroupLabel());
+				if ( item_group != null ){
+					boolean isRepeatingGroup=item_group.getMeta().isRepeatingGroup();
+					if ( isRepeatingGroup){
+			            if ( this.getGroupLabel()!= null && this.getGroupLabel().length()>0){
+			            	ver_errors.add(resPageMsg.getString("parentId_group")+row_items.size()+resPageMsg.getString("nested_parent_id_1"));
+			            	 htmlErrors.put(sheetNumber + "," + row_number + ","+CrfTemplateColumnNameEnum.PARENT_ITEM.getCellNumber(), resPageMsg.getString("INVALID_FIELD"));
+			            }
+					}
+				}
             }
         }
         
-        
-		return ver_errors;
 	}
+	
+	public void verifySectionLabel(ArrayList< SpreadSheetItemUtil> row_items, 
+			ArrayList<String> ver_errors, ArrayList<String> secNames,
+			HashMap<String,String> htmlErrors, int sheetNumber,  ResourceBundle resPageMsg){
+		int row_number=row_items.size();
+		StringBuffer str= new StringBuffer();
+		if ( this.getSectionLabel() == null || this.getSectionLabel().length()==0){
+			str.append(resPageMsg.getString("the") + " ");
+			str.append( resPageMsg.getString("SECTION_LABEL_column") + " ");
+			str.append( resPageMsg.getString("not_valid_section_at_row") + " ");
+			str.append( row_number + ", " + resPageMsg.getString("items_worksheet_with_dot")) ;
+			str.append(" "+ resPageMsg.getString("check_to_see_that_there_is_valid_LABEL"));
+			ver_errors.add(str.toString());
+            htmlErrors.put(sheetNumber + "," + row_number + ","+ CrfTemplateColumnNameEnum.SECTION_LABEL.getCellNumber(), 
+            		resPageMsg.getString("NOT_A_VALID_LABEL"));
+       }
+		
+       if (this.getSectionLabel() != null && this.getSectionLabel().length() > 2000) {
+    	   ver_errors.add(resPageMsg.getString("section_label_length_error"));
+    	   htmlErrors.put(sheetNumber + "," + row_number + ","+ CrfTemplateColumnNameEnum.SECTION_LABEL.getCellNumber(), 
+           		resPageMsg.getString("NOT_A_VALID_LABEL"));
+       }
+
+		if (!secNames.contains(this.getSectionLabel())) {
+			if ( str.length()==0){
+				str.append(resPageMsg.getString("the") + " ");
+				str.append( resPageMsg.getString("SECTION_LABEL_column") + " ");
+				str.append( resPageMsg.getString("not_valid_section_at_row") + " ");
+				str.append( row_number + ", " + resPageMsg.getString("items_worksheet_with_dot")) ;
+				str.append(" "+ resPageMsg.getString("check_to_see_that_there_is_valid_LABEL"));
+			}
+			ver_errors.add(str.toString());
+	            
+			htmlErrors.put(sheetNumber + "," + row_number + ","+ CrfTemplateColumnNameEnum.SECTION_LABEL.getCellNumber(),
+					resPageMsg.getString("NOT_A_VALID_LABEL"));
+	     }
+
+	}
+	public void verifyItemName(ArrayList< SpreadSheetItemUtil> row_items, ArrayList<String> ver_errors, 
+			HashMap<String,String> htmlErrors, int sheetNumber,  ResourceBundle resPageMsg){
+		
+		int k = row_items.size();
+		String itemName = this.getItemName();
+		  // regexp to make sure it is all word characters, '\w+' in regexp terms
+        if (!Utils.isMatchingRegexp(itemName, "\\w+")) {
+            // different item error to go here
+        	ver_errors.add(resPageMsg.getString("item_name_column") + " " + resPageMsg.getString("was_invalid_at_row") + " " + k + ", "
+                    + resPageMsg.getString("items_worksheet_with_dot")  + " "+resPageMsg.getString("you_can_only_use_letters_or_numbers"));
+                htmlErrors.put(sheetNumber + "," + k + ",0", resPageMsg.getString("INVALID_FIELD"));
+        }
+        if (StringUtil.isBlank(itemName)) {
+        	ver_errors.add(resPageMsg.getString("the") + " " + resPageMsg.getString("item_name_column") + " "
+                + resPageMsg.getString("was_blank_at_row") + " "+k + ", " + resPageMsg.getString("items_worksheet_with_dot") );
+            htmlErrors.put(sheetNumber + "," + k + ","+ CrfTemplateColumnNameEnum.ITEM_NAME.getCellNumber()
+                , resPageMsg.getString("required_field"));
+        }
+        if (itemName != null && itemName.length() > 255) {
+        	ver_errors.add(resPageMsg.getString("item_name_length_error"));
+        }
+
+        if (SpreadSheetItemUtil.isItemWithSameParameterExists(itemName, row_items)) {
+            // errors.add("A duplicate ITEM_NAME of " + itemName
+            // + " was detected at row " + k
+            // + ", Items worksheet.");
+        	ver_errors.add(resPageMsg.getString("duplicate") + " " + resPageMsg.getString("item_name_column") + " " + itemName + " "
+                + resPageMsg.getString("was_detected_at_row") + " " + k + ", " + resPageMsg.getString("items_worksheet_with_dot") );
+            htmlErrors.put(sheetNumber + "," + k + ","+CrfTemplateColumnNameEnum.ITEM_NAME.getCellNumber(),
+            		resPageMsg.getString("required_field"));
+        }
+		
+	}
+
+	public void verifyDefaultValue( ArrayList< SpreadSheetItemUtil> row_items,
+			ArrayList<String> ver_errors, 
+			HashMap<String,String> htmlErrors, int sheetNumber,  ResourceBundle resPageMsg){
+		
+		int row_number = row_items.size();
+		
+		 if ("date".equalsIgnoreCase(this.getDataType()) && !"".equals(this.getDefaultValue())) {
+             // BWP>> try block needs to be added, because
+             // cell.getDateCellValue()
+             // can throw an exception.
+             // All database values are stored in this format? en
+             // Locale MM/dd/yyyy
+             try {
+            	 this.setDefaultValue( new SimpleDateFormat(ApplicationConstants.getDateFormatInItemData()).format(this.getDefaultValue()));
+             } catch (Exception e) {
+            	 this.setDefaultValue("");
+            	 //TODO raise exception
+             }
+         }
+         if (this.getDefaultValue().length() > 0) {
+             if(this.getResponseTypeId() == ResponseType.CALCULATION.getId()
+                 || this.getResponseTypeId() == ResponseType.GROUP_CALCULATION.getId()
+                 || this.getResponseTypeId() == ResponseType.FILE.getId()
+                 || this.getResponseTypeId() == ResponseType.INSTANT_CALCULATION.getId()) {
+            	 ver_errors.add(resPageMsg.getString("default_value_not_allowed") + this.getItemName() +" "+resPageMsg.getString("change_radio") + " "+resPageMsg.getString("items_worksheet_with_dot"));
+                 htmlErrors.put(sheetNumber + "," + row_number + ","+CrfTemplateColumnNameEnum.DEFAULT_VALUE.getCellNumber()
+            	 , resPageMsg.getString("INVALID_FIELD"));
+             } 
+            	 
+             //do not allow more than one value as a default value, value should be from response types
+             else if(this.getResponseTypeId() == ResponseType.SELECT.getId()) {
+            	 if( this.getDefaultValue().indexOf(',')!=-1){
+            		 ver_errors.add(resPageMsg.getString("default_value_wrong_select") + row_number + ", " + resPageMsg.getString("items_worksheet_with_dot"));
+                     htmlErrors.put(sheetNumber + "," + row_number + ","+CrfTemplateColumnNameEnum.DEFAULT_VALUE.getCellNumber()
+                	 , resPageMsg.getString("INVALID_FIELD"));
+            	 }
+            	 //more logic should be here: current implementation supports DEFAULT_LABEL, several non - in-response values
+//            	 if (response_options!=null && response_options.length>0){
+//            		 boolean flagDefaultValueVerified=false;
+//            		 for (String cur_option: response_options){
+//            			 if (cur_option.trim().equals(this.getDefaultValue())){
+//            				 flagDefaultValueVerified=true;
+//            				 break;
+//            			 }
+//            		 }
+//            		 if (!flagDefaultValueVerified){
+//            			 ver_errors.add(resPageMsg.getString("default_value_wrong_select_1") + row_number + ", " + resPageMsg.getString("items_worksheet_with_dot"));
+//                         htmlErrors.put(sheetNumber + "," + row_number + ","+CrfTemplateColumnNameEnum.DEFAULT_VALUE.getCellNumber()
+//                    	 , resPageMsg.getString("INVALID_FIELD"));
+//            		 }
+//            	 }
+            	 
+             }
+             else if(this.getResponseTypeId() == ResponseType.CHECKBOX.getId()||
+            		 this.getResponseTypeId() == ResponseType.SELECTMULTI.getId()){
+            	 //TODO
+             }
+           
+         }
+	}
+	
+
+	/**
+	 * @return the response_options
+	 */
+	public String[] getResponseOptions() {
+		return response_options;
+	}
+
+	/**
+	 * @param response_options the response_options to set
+	 */
+	public void setResponseOptions(String[] response_options) {
+		this.response_options = response_options;
+	}
+	
 }
