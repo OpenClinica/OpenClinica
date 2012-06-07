641c87dfe038f5c57c888a957594d30e09c5d2fb
Douglas Rodrigues <drodrigues@openclinica.com>
2012-06-07 19:14:45 -0400

    [11951] Stopwatch now uses commons-lang's StopWatch

--- core/src/main/java/org/akaza/openclinica/log/Stopwatch.java
+++ core/src/main/java/org/akaza/openclinica/log/Stopwatch.java
@@ -18,14 +18,14 @@ import org.slf4j.LoggerFactory;
  */
 public class Stopwatch {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Stopwatch.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(Stopwatch.class);
 
     private static final String START_LABEL = "START: %s";
     private static final String STOP_LABEL  = "STOP:  %s - %dm %.2fs";
 
     private final String name;
 
-    private long startTime;
+    private final org.apache.commons.lang.time.StopWatch stopwatch = new org.apache.commons.lang.time.StopWatch();
 
     public static Stopwatch createAndStart(String name) {
         Stopwatch result = new Stopwatch(name);
@@ -39,14 +39,15 @@ public class Stopwatch {
 
     public void start() {
         if (LOG.isDebugEnabled()) {
-            startTime = System.currentTimeMillis();
+            stopwatch.start();
             LOG.debug(String.format(START_LABEL, name));
         }
     }
 
     public void stop() {
         if (LOG.isDebugEnabled()) {
-            long totalTime = System.currentTimeMillis() - startTime;
+            stopwatch.stop();
+            long totalTime = stopwatch.getTime();
             LOG.debug(String.format(STOP_LABEL, name, totalTime / 60000,  (float) (totalTime % 60000) / 1000));
         }
     }
