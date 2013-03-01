86d30ad55e4397200b67b45e800519c4a52bfac7
Jamuna Nyayapathi <jnyayapathi@openclinica.com>
2013-03-01 11:35:05 -0500

    Backed out changeset: ff930b85185b

--- web/src/main/webapp/js/com/openclinica/renderer/FormDefRenderer.js
+++
@@ -1,8 +0,0 @@
-function FormDefRenderer(json) {
-  this.json = json;
-  this.name = json["@Name"];
-  this.OID = json["@OID"];
-  this.repeating = ParseUtil.parseYesNo(json["@Repeating"]);
-  this.renderPrintableForm = function() { return RenderUtil.render(RenderUtil.get("print_form_def"), {name: this.name});}
-  this.renderInteractiveForm = function() { return RenderUtil.render(RenderUtil.get("e_form_def"), {name: this.name});}
-}
