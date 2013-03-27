ec64972e054989147e446dc6af5ff5055b814ca9
Nicholas Sophinos <nick@wdeanmedical.com>
2013-03-27 14:37:50 -0400

    The loading message is now removed when the json is returned to the client instead upon successful rendering of the printable form.

--- web/src/main/webapp/js/app.js
+++ web/src/main/webapp/js/app.js
@@ -31,11 +31,11 @@ $(document).ready(function() {
 function getPrintableContent() {
     $('.spinner').css({display: "block"});
   $.get(app_contextPath + '/rest/metadata/json/view/' + app_studyOID, {}, function(data) {
+    $('.spinner').css({display: "none"});
     $('#form_wrapper').css({display: "block"});
     setRenderMode();
     app_odmRenderer = new ODMRenderer(data);
     $('#form_wrapper').html(app_odmRenderer.renderPrintableForm(renderMode));
-    $('.spinner').css({display: "none"});
    });
 }
 
