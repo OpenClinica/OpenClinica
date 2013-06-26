32e5fc79d6a868a6121ce8505cfbe97f99167c8a
Nicholas Sophinos <nick@wdeanmedical.com>
2013-06-26 19:43:21 -0400

    Work on server-side printcrf free marker templates for populated crfs

---
+++ web/src/main/java/org/akaza/openclinica/renderer/StudyDataLoader.java
@@ -0,0 +1,20 @@
+package org.akaza.openclinica.renderer;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import freemarker.template.Configuration;
+
+import net.sf.json.JSON;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
+
+public class StudyDataLoader {
+
+
+  
+
+}
--- web/src/main/java/org/akaza/openclinica/renderer/StudyRenderer.java
+++ web/src/main/java/org/akaza/openclinica/renderer/StudyRenderer.java
@@ -16,13 +16,19 @@ public class StudyRenderer extends JSONRenderer{
 
 
   private String OID;
-  
+  private String renderString = "";
   private FormDefRenderer formDefRenderer;
   private ItemDefRenderer itemDefRenderer;
+  private StudyDataLoader studyDataLoader;
+  private PageHeaderRenderer pageHeaderRenderer;
   public  static Map<String, String> appBasicDefinitions = new TreeMap<String,String>();
   public  static Map<String, List> appCodeLists = new TreeMap<String,List>();
   private  Map<String, ItemGroup> appItemGroupDefs = new TreeMap<String,ItemGroup>();
   private  Map<String, String> appItemGroupMap = new TreeMap<String,String>();
+  
+  public static final int DEFAULT_MAX_REPEAT = 40; 
+  public static final boolean NO_PAGE_BREAK = false; 
+  public static final boolean PAGE_BREAK = true; 
   
   
   public StudyRenderer(JSON json, Configuration cfg, Map templateVars) {
