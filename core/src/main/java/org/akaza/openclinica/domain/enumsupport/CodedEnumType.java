c3295e0090c345663deab30c5b5de3b3bf198077
Jamuna Nyayapathi <jnyayapathi@openclinica.com>
2013-07-17 15:51:03 -0400

    OC-3128 Hibernate upgrade

--- core/src/main/java/org/akaza/openclinica/domain/enumsupport/CodedEnumType.java
+++ core/src/main/java/org/akaza/openclinica/domain/enumsupport/CodedEnumType.java
@@ -2,9 +2,11 @@ package org.akaza.openclinica.domain.enumsupport;
 
 import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.type.StandardBasicTypes;
 import org.hibernate.usertype.EnhancedUserType;
 import org.hibernate.usertype.ParameterizedType;
-import org.hibernate.util.ReflectHelper;
+//import org.hibernate.util.ReflectHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,7 +39,7 @@ public class CodedEnumType implements EnhancedUserType, ParameterizedType {
     public void setParameterValues(Properties parameters) {
         String enumClassName = parameters.getProperty("enumClassname");
         try {
-            enumClass = ReflectHelper.classForName(enumClassName);
+            enumClass = org.hibernate.internal.util.ReflectHelper.classForName(enumClassName);
         } catch (ClassNotFoundException cnfe) {
             throw new HibernateException("Enum class not found", cnfe);
         }
@@ -57,7 +59,7 @@ public class CodedEnumType implements EnhancedUserType, ParameterizedType {
      * 
      */
     public int[] sqlTypes() {
-        return new int[] { Hibernate.INTEGER.sqlType() };
+        return new int[] { StandardBasicTypes.INTEGER.sqlType() };
     }
 
     /* 
@@ -139,7 +141,7 @@ public class CodedEnumType implements EnhancedUserType, ParameterizedType {
      */
     public void nullSafeSet(PreparedStatement st, Object value, int index) throws SQLException {
         if (value == null) {
-            st.setNull(index, Hibernate.INTEGER.sqlType());
+            st.setNull(index, StandardBasicTypes.INTEGER.sqlType());
         } else {
             Integer code = getCode(value);
             logger.debug("Binding '{}' to parameter: {}", code, index);
@@ -179,5 +181,20 @@ public class CodedEnumType implements EnhancedUserType, ParameterizedType {
         }
         return value;
     }
+
+	@Override
+	public Object nullSafeGet(ResultSet rs, String[] names,
+			SessionImplementor session, Object owner)
+			throws HibernateException, SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void nullSafeSet(PreparedStatement st, Object value, int index,
+			SessionImplementor session) throws HibernateException, SQLException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
