1d0e73cec9f68f3f8be1afaad7300a43a0e636f2
Joe Keremian <jkeremian@openclinica.com>
2014-01-06 16:46:18 -0500

    OC-3578 Change DN Last updated label into Created label OC-4094   Updated Discrepancy Notes reports for CSS changes

--- web/src/main/webapp/js/com/openclinica/renderer/StudyRenderer.js
+++ web/src/main/webapp/js/com/openclinica/renderer/StudyRenderer.js
@@ -995,17 +995,18 @@ function StudyRenderer(json) {
 					// var discrepancyNote=[];
 					var currentDiscrepancyNotes = [];
 					var thisDiscrepancyNote = {};
-					var parentDiscrepancyNote = {};
+					var parentDiscrepancyNote;
 					var childNote = [];
 					for ( var i = 0; i < discrepancyNote.length; i++) {
 						var dns = discrepancyNote[i];
-
+						parentDiscrepancyNote = {};
 						parentDiscrepancyNote.description = "ABC";
 						parentDiscrepancyNote.id = dns["@ID"].substring(3);
 						parentDiscrepancyNote.parent_noteType = dns["@NoteType"];
 						parentDiscrepancyNote.status = dns["@Status"];
 						parentDiscrepancyNote.numberOfChildNotes = dns["@NumberOfChildNotes"];
 						parentDiscrepancyNote.dateUpdated = dns["@DateUpdated"];
+						
 
 						childNote = dns["OpenClinica:ChildNote"];
 						childNote = util_ensureArray(childNote);
@@ -1014,9 +1015,14 @@ function StudyRenderer(json) {
 						for ( var j = 0; j < childNote.length; j++) {
 							var cn = childNote[j];
 							var description = cn["OpenClinica:Description"];
+
 							if(j==0)
 							{	parentDiscrepancyNote.description = description;
+							    parentDiscrepancyNote.parentThread = "true";
 								currentDiscrepancyNotes.push(parentDiscrepancyNote);
+							}else{
+							    parentDiscrepancyNote.parentThread = "false";
+									
 							}
 
 							var detailedNote = cn["OpenClinica:DetailedNote"];
@@ -1109,16 +1115,18 @@ function StudyRenderer(json) {
 				// var discrepancyNote=[];
 				var currentDiscrepancyNotes = [];
 				var thisDiscrepancyNote = {};
+				var parentDiscrepancyNote;
 				var childNote = [];
 				for ( var i = 0; i < discrepancyNote.length; i++) {
 					var dns = discrepancyNote[i];
 
-					thisDiscrepancyNote.description = "";
-					thisDiscrepancyNote.id = dns["@ID"].substring(3);
-					thisDiscrepancyNote.parent_noteType = dns["@NoteType"];
-					thisDiscrepancyNote.status = dns["@Status"];
-					thisDiscrepancyNote.numberOfChildNotes = dns["@NumberOfChildNotes"];
-					thisDiscrepancyNote.dateUpdated = dns["@DateUpdated"];
+					parentDiscrepancyNote = {};
+					parentDiscrepancyNote.description = "ABC";
+					parentDiscrepancyNote.id = dns["@ID"].substring(3);
+					parentDiscrepancyNote.parent_noteType = dns["@NoteType"];
+					parentDiscrepancyNote.status = dns["@Status"];
+					parentDiscrepancyNote.numberOfChildNotes = dns["@NumberOfChildNotes"];
+					parentDiscrepancyNote.dateUpdated = dns["@DateUpdated"];
 
 					childNote = dns["OpenClinica:ChildNote"];
 					childNote = util_ensureArray(childNote);
@@ -1128,6 +1136,20 @@ function StudyRenderer(json) {
 					for ( var j = 0; j < childNote.length; j++) {
 						var cn = childNote[j];
 						var description = cn["OpenClinica:Description"];
+						
+						
+						if(j==0)
+						{	parentDiscrepancyNote.description = description;
+						    parentDiscrepancyNote.parentThread = "true";
+							currentDiscrepancyNotes.push(parentDiscrepancyNote);
+						}else{
+						    parentDiscrepancyNote.parentThread = "false";
+								
+						}
+
+						
+						
+						
 						var detailedNote = cn["OpenClinica:DetailedNote"];
 						var userRef = cn["UserRef"];
 						thisDiscrepancyNote.description = description;
