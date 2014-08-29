d40ef27332beaa5c27634a77b404b0a403d59910
Joe Keremian <jkeremian@openclinica.com>
2014-08-29 16:30:37 -0400

    OC-5897
    Recording of the Study Subject record, CRUD in Audit log

--- core/src/main/java/org/akaza/openclinica/dao/managestudy/StudySubjectDAO.java
+++ core/src/main/java/org/akaza/openclinica/dao/managestudy/StudySubjectDAO.java
@@ -1060,8 +1060,9 @@ public class StudySubjectDAO<K extends String,V extends ArrayList> extends Audit
             ind++;
         }
 
-        variables.put(new Integer(ind), new java.util.Date());
-        ind++;
+     // Date_updated is now()
+      //  variables.put(new Integer(ind), new java.util.Date());
+      //  ind++;
         variables.put(new Integer(ind), new Integer(sb.getUpdater().getId()));
         ind++;
         variables.put(new Integer(ind), sb.getSecondaryLabel());
