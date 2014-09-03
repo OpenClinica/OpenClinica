268744d8f33ee4c11711dc4b921e7960e5938cca
sdibona <sdibona@openclinica.com>
2014-09-03 16:54:46 -0400

    OC-5864 - Display Pform when DM clicks on PForm link (PForm is hard coded)
    Changed API call to Enketo to use "preview" rather than "survey".

--- web/src/main/java/org/akaza/openclinica/web/pform/EnketoAPI.java
+++ web/src/main/java/org/akaza/openclinica/web/pform/EnketoAPI.java
@@ -76,4 +76,54 @@ public class EnketoAPI {
 		return crfURL;
 	}
 	
+	public String getFormPreviewURL(String crfOID) throws Exception
+	{
+		String crfURL = "";
+
+		try
+		{
+			URL eURL = new URL(enketoURL + "/survey/preview");
+			HttpURLConnection con = (HttpURLConnection)eURL.openConnection();
+			String userPasswdCombo = new String(Base64.encodeBase64((token + ":").getBytes()));
+			
+			con.setConnectTimeout(5000);
+			con.setReadTimeout(5000);
+			con.setRequestMethod("POST");			
+			con.setRequestProperty("Authorization", "Basic " + userPasswdCombo);
+			con.setRequestProperty("Accept-Charset", "UTF-8");
+			con.setDoOutput(true); 
+			con.setDoInput(true);	
+	
+			String openClinicaURL = "server_url=" + ocURL + "&form_id=" + crfOID;
+			DataOutputStream output = new DataOutputStream(con.getOutputStream());  
+			output.writeBytes(openClinicaURL);
+			output.flush();
+			output.close();
+		      	
+			BufferedReader inputReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String currLine = null;
+			while ((currLine = inputReader.readLine()) != null)
+			{		
+				if (currLine.contains(":"))
+				{
+					String tokenName = currLine.split(":")[0];
+					if (tokenName.contains("url")) crfURL = currLine.substring(currLine.indexOf(":")+3,currLine.length()-2);
+					else if (tokenName.contains("code"))
+					{
+						String code = currLine.split(":")[1];
+						if (Integer.valueOf(code.trim()) > 299) throw new Exception("Error retrieving Enketo URL.  Received error code: " + code);
+					}			
+				}
+			}
+			inputReader.close();
+		}
+		catch (Exception e)
+		{
+			logger.debug(e.getMessage());
+			logger.debug(ExceptionUtils.getStackTrace(e));
+		}
+		
+		return crfURL;
+	}
+	
 }
