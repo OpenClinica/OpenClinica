a4e410336c0c7ab29d3bcac1363af44f9f21a7fb
sdibona <sdibona@openclinica.com>
2014-09-23 14:33:33 -0400

    OC-5928: REST URL to use studySubjectID to get clinical data
    Added code to handle a SSID as well as a SSOID in the REST calls.

--- web/src/main/java/org/akaza/openclinica/web/restful/ODMClinicaDataResource.java
+++ web/src/main/java/org/akaza/openclinica/web/restful/ODMClinicaDataResource.java
@@ -2,6 +2,7 @@ package org.akaza.openclinica.web.restful;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -17,6 +18,11 @@ import net.sf.json.xml.XMLSerializer;
 
 import org.akaza.openclinica.bean.extract.odm.FullReportBean;
 import org.akaza.openclinica.bean.login.UserAccountBean;
+import org.akaza.openclinica.bean.managestudy.StudyBean;
+import org.akaza.openclinica.bean.managestudy.StudySubjectBean;
+import org.akaza.openclinica.dao.managestudy.StudyDAO;
+import org.akaza.openclinica.dao.managestudy.StudySubjectDAO;
+import org.akaza.openclinica.dao.submit.SubjectDAO;
 import org.akaza.openclinica.i18n.core.LocaleResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,18 +48,10 @@ public class ODMClinicaDataResource {
 			.getLogger(ODMClinicaDataResource.class);
 	private static final int INDENT_LEVEL = 2;
 
-	public ClinicalDataCollectorResource getClinicalDataCollectorResource() {
-		return clinicalDataCollectorResource;
-	}
-
-	public void setClinicalDataCollectorResource(
-			ClinicalDataCollectorResource clinicalDataCollectorResource) {
-		this.clinicalDataCollectorResource = clinicalDataCollectorResource;
-	}
 
 	private ClinicalDataCollectorResource clinicalDataCollectorResource;
-
 	private MetadataCollectorResource metadataCollectorResource;
+	private DataSource dataSource;
 
 	public MetadataCollectorResource getMetadataCollectorResource() {
 		return metadataCollectorResource;
@@ -63,14 +61,31 @@ public class ODMClinicaDataResource {
 			MetadataCollectorResource metadataCollectorResource) {
 		this.metadataCollectorResource = metadataCollectorResource;
 	}
+	
+	public ClinicalDataCollectorResource getClinicalDataCollectorResource() {
+		return clinicalDataCollectorResource;
+	}
+
+	public void setClinicalDataCollectorResource(
+			ClinicalDataCollectorResource clinicalDataCollectorResource) {
+		this.clinicalDataCollectorResource = clinicalDataCollectorResource;
+	}
+
+	public DataSource getDataSource() {
+		return dataSource;
+	}
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	@GET
-	@Path("/json/view/{studyOID}/{studySubjectOID}/{studyEventOID}/{formVersionOID}")
+	@Path("/json/view/{studyOID}/{studySubjectIdentifier}/{studyEventOID}/{formVersionOID}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getODMClinicaldata(@PathParam("studyOID") String studyOID,
 			@PathParam("formVersionOID") String formVersionOID,
 			@PathParam("studyEventOID") String studyEventOID,
-			@PathParam("studySubjectOID") String studySubjOID,
+			@PathParam("studySubjectIdentifier") String studySubjectIdentifier,
 			@DefaultValue("n") @QueryParam("includeDNs") String includeDns,
 			@DefaultValue("n") @QueryParam("includeAudits") String includeAudits,@Context HttpServletRequest request) {
 		LOGGER.debug("Requesting clinical data resource");
@@ -85,7 +100,7 @@ public class ODMClinicaDataResource {
 		FullReportBean report = getMetadataCollectorResource().collectODMMetadataForClinicalData(studyOID,
 						formVersionOID,
 						getClinicalDataCollectorResource()
-								.generateClinicalData(studyOID, studySubjOID,
+								.generateClinicalData(studyOID, getStudySubjectOID(studySubjectIdentifier,studyOID),
 										studyEventOID, formVersionOID,includeDN,includeAudit,request.getLocale(), userId));
 		report.createOdmXml(true);
 		//xmlSerializer.setForceTopLevelObject(true);
@@ -102,18 +117,18 @@ public class ODMClinicaDataResource {
 	}
 
 	@GET
-	@Path("/html/print/{studyOID}/{studySubjectOID}/{eventOID}/{formVersionOID}")
+	@Path("/html/print/{studyOID}/{studySubjectIdentifier}/{eventOID}/{formVersionOID}")
 	public Viewable getPrintCRFController(@Context HttpServletRequest request,
 			@Context HttpServletResponse response,
 			@PathParam("studyOID") String studyOID,
-			@PathParam("studySubjectOID") String studySubjectOID,
+			@PathParam("studySubjectIdentifier") String studySubjectIdentifier,
 			@PathParam("eventOID") String eventOID,
 			@PathParam("formVersionOID") String formVersionOID,	@DefaultValue("n") @QueryParam("includeDNs") String includeDns,
 			@DefaultValue("n") @QueryParam("includeAudits") String includeAudits)
 			throws Exception {
 		request.setCharacterEncoding("UTF-8");
 		request.setAttribute("studyOID", studyOID);
-		request.setAttribute("studySubjectOID", studySubjectOID);
+		request.setAttribute("studySubjectOID", getStudySubjectOID(studySubjectIdentifier,studyOID));
 		request.setAttribute("eventOID", eventOID);
 		request.setAttribute("formVersionOID", formVersionOID);
 		request.setAttribute("includeAudits", includeAudits);
@@ -122,11 +137,11 @@ public class ODMClinicaDataResource {
 	}
 
 	@GET
-	@Path("/xml/view/{studyOID}/{studySubjectOID}/{studyEventOID}/{formVersionOID}")
+	@Path("/xml/view/{studyOID}/{studySubjectIdentifier}/{studyEventOID}/{formVersionOID}")
 	@Produces(MediaType.TEXT_XML)
 	public String getODMMetadata(@PathParam("studyOID") String studyOID,
 			@PathParam("formVersionOID") String formVersionOID,
-			@PathParam("studySubjectOID") String studySubjOID,
+			@PathParam("studySubjectIdentifier") String studySubjectIdentifier,
 			@PathParam("studyEventOID") String studyEventOID,
 			@DefaultValue("n") @QueryParam("includeDNs") String includeDns,
 			@DefaultValue("n") @QueryParam("includeAudits") String includeAudits,@Context HttpServletRequest request) {
@@ -144,13 +159,26 @@ public class ODMClinicaDataResource {
 						studyOID,
 						formVersionOID,
 						getClinicalDataCollectorResource()
-								.generateClinicalData(studyOID, studySubjOID,
+								.generateClinicalData(studyOID, getStudySubjectOID(studySubjectIdentifier,studyOID),
 										studyEventOID, formVersionOID,includeDN,includeAudit,request.getLocale(), userId));
 
 		report.createOdmXml(true);
 		LOGGER.debug(report.getXmlOutput().toString().trim());
 
 		return report.getXmlOutput().toString().trim();
+	}
+	
+	private String getStudySubjectOID(String subjectIdentifier, String studyOID)
+	{
+		StudySubjectDAO studySubjectDAO = new StudySubjectDAO(getDataSource());
+		StudySubjectBean studySubject = studySubjectDAO.findByOid(subjectIdentifier);
+		if (studySubject != null  && studySubject.getOid() != null) return studySubject.getOid();
+		else 
+		{
+			StudyDAO studyDAO = new StudyDAO(getDataSource());
+			StudyBean study = studyDAO.findByOid(studyOID); 
+			return studySubjectDAO.findByLabelAndStudy(subjectIdentifier,study).getOid();
+		}
 	}
 
 }
