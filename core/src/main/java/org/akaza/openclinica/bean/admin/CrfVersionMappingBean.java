333ccfd5b95b3f38f7cbddeee5c4ab0a540ade18
jkeremian <jkeremian@openclinica.com>
2014-10-26 21:31:20 -0400

    OC-5990 adding new Beans

---
+++ core/src/main/java/org/akaza/openclinica/bean/admin/CrfVersionMappingBean.java
@@ -0,0 +1,21 @@
+package org.akaza.openclinica.bean.admin;
+
+public class CrfVersionMappingBean {
+	
+	private Integer crfVersionId;
+	private Integer eventCrfId;
+	public Integer getCrfVersionId() {
+		return crfVersionId;
+	}
+	public void setCrfVersionId(Integer crfVersionId) {
+		this.crfVersionId = crfVersionId;
+	}
+	public Integer getEventCrfId() {
+		return eventCrfId;
+	}
+	public void setEventCrfId(Integer eventCrfId) {
+		this.eventCrfId = eventCrfId;
+	}
+
+	
+}
