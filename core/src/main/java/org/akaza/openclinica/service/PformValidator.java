06662d21efe572fe5a31c526ccc009ff47763ccd
jkeremian <jkeremian@openclinica.com>
2014-11-18 13:35:22 -0500

    OC-5826 Wrote a unit test to test checkbox and multi select response type for different item data type ids.

--- core/src/main/java/org/akaza/openclinica/service/PformValidator.java
+++ core/src/main/java/org/akaza/openclinica/service/PformValidator.java
@@ -14,76 +14,74 @@ public class PformValidator implements Validator {
 		return ItemItemDataContainer.class.equals(clazz);
 	}
 
-
-
 	@Override
-	public void  validate(Object target, Errors e) {
+	public void validate(Object target, Errors e) {
 		ItemItemDataContainer container = (ItemItemDataContainer) target;
-		String origValue=container.getItemDataBean().getValue();
-	    Integer responseTypeId= container.getResponseTypeId();
-        Integer itemDataTypeId =container.getItemBean().getItemDataTypeId();
-        System.out.println("item data type id:  "+itemDataTypeId);
-        
-	    if (responseTypeId==3 || responseTypeId==7){
-            String[] values = origValue.split(",");            
-            for(String value : values){
-            	subValidator(itemDataTypeId, value, e);
-                System.out.println(value);        
-            }
-            }else{
-            	subValidator(itemDataTypeId, origValue, e);            	
-            }
+		String origValue = container.getItemDataBean().getValue();
+		Integer responseTypeId = container.getResponseTypeId();
+		Integer itemDataTypeId = container.getItemBean().getItemDataTypeId();
+		System.out.println();
+		System.out.print("Data type id:  " + itemDataTypeId);
+
+		if (responseTypeId == 3 || responseTypeId == 7) {
+			String[] values = origValue.split(",");
+			for (String value : values) {
+				subValidator(itemDataTypeId, value.trim(), e);
+				System.out.print(" " + value);
+			}
+		} else {
+			subValidator(itemDataTypeId, origValue, e);
+			System.out.print(" " + origValue);
+
+		}
 	}
-	
-	
- public void subValidator(Integer itemDataTypeId, String value , Errors e){	
-        if (value != null &&  value != "") {
-			
+
+	public void subValidator(Integer itemDataTypeId, String value, Errors e) {
+		if (value != null && value != "") {
+
 			switch (itemDataTypeId) {
-	        case 6: { //ItemDataType.INTEGER
-	            try {	 Integer.valueOf(value);
-	                    System.out.println("I'm in Integer data type");
-	            } catch (NumberFormatException nfe) {
-	                e.reject("value.invalid.Integer");
-	            }
-	            break;
-	        }
-	        case 7: { //ItemDataType.REAL
-	            try {
-	                Float.valueOf(value);
-                    System.out.println("I'm in Real data type");
-	            } catch (NumberFormatException nfe) {
-	                e.reject("value.invalid.float");
-	            }
-	            break;
-	        }
-	        case 9: { //ItemDataType.DATE
-                System.out.println("I'm in Date data type");
-	            if (!ExpressionTreeHelper.isDateyyyyMMddDashes(value)) {
-	                e.reject("value.invalid.date");
-	            }
-	            break;
-	        }
-	        case 10: { //ItemDataType.PDATE
-	        	e.reject("value.notSupported.pdate");
-	            break;
-	        }
-	        case 11: { //ItemDataType.FILE
-	            e.reject("value.notSupported.file");
-	            break;
-	        }
+			case 6: { // ItemDataType.INTEGER
+				try {
+					Integer.valueOf(value);
+					System.out.print(" Integer type");
+				} catch (NumberFormatException nfe) {
+					System.out.print(" Error");
+					e.reject("value.invalid.Integer");
+				}
+				break;
+			}
+			case 7: { // ItemDataType.REAL
+				try {
+					Float.valueOf(value);
+					System.out.print(" Real type");
+				} catch (NumberFormatException nfe) {
+					System.out.print(" Error");
+					e.reject("value.invalid.float");
+				}
+				break;
+			}
+			case 9: { // ItemDataType.DATE
+				System.out.print("  Date type");
+				if (!ExpressionTreeHelper.isDateyyyyMMddDashes(value)) {
+					System.out.print(" Error");
+					e.reject("value.invalid.date");
+				}
+				break;
+			}
+			case 10: { // ItemDataType.PDATE
+				e.reject("value.notSupported.pdate");
+				break;
+			}
+			case 11: { // ItemDataType.FILE
+				e.reject("value.notSupported.file");
+				break;
+			}
 
-	        default:
-	            break;
-	        }
+			default:
+				break;
+			}
 
-	    }
+		}
 	}
-        
-        
-        
-        
-        
-        
-	
+
 }
