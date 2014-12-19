673615b2cad8d523d8a9e623b58f7449ac3fc241
jkeremian <jkeremian@openclinica.com>
2014-12-19 14:57:29 -0500

    OC-6110 check in fix code for Current Date format . We can now use 'eq' operator and other with Current date function

--- web/src/main/java/org/akaza/openclinica/web/pform/OpenRosaXmlGenerator.java
+++ web/src/main/java/org/akaza/openclinica/web/pform/OpenRosaXmlGenerator.java
@@ -583,7 +583,7 @@ public class OpenRosaXmlGenerator {
 		String result = "";
 		expression = " " + expression;
 		expression = expression.replaceAll("\\(", "\\( ");
-		expression = expression.replaceAll("_CURRENT_DATE", " today() ");
+		expression = expression.replaceAll("_CURRENT_DATE", " format-date(today(), \"%Y-%n-%e\") ");
 		/*
 		 * today() function returns date and time and will not work with 'eq'
 		 * operator. But it will work with 'gt' or 'lt' operators
