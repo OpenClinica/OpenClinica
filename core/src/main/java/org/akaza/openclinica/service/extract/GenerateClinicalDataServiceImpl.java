4cabaf692276f6ff827f4cba01b7781a82a24142
jkeremian <jkeremian@openclinica.com>
2015-01-11 23:25:46 -0500

    OC-6008 fix for issue OC 5416  REST output gives duplicate values for CRF with more than one version

--- core/src/main/java/org/akaza/openclinica/service/extract/GenerateClinicalDataServiceImpl.java
+++ core/src/main/java/org/akaza/openclinica/service/extract/GenerateClinicalDataServiceImpl.java
@@ -274,11 +274,12 @@ public class GenerateClinicalDataServiceImpl implements GenerateClinicalDataServ
 	}
 
 	private ArrayList<ExportStudyEventDataBean> setExportStudyEventDataBean(
-			StudySubject ss,List<StudyEvent>studyEvents,String formVersionOID) {
+			StudySubject ss,List<StudyEvent>sEvents,String formVersionOID) {
 		ArrayList<ExportStudyEventDataBean> al = new ArrayList<ExportStudyEventDataBean>();
-
+           
+		  
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		for (StudyEvent se : studyEvents) {
+		for (StudyEvent se : ss.getStudyEvents()) {
 			if(se!=null){
 			ExportStudyEventDataBean expSEBean = new ExportStudyEventDataBean();
 			
