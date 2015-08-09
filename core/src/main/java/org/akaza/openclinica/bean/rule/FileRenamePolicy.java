a70229f580af3ea13ffd984a94041bddca7f6d40
Ron Panduwana <panduwana@gmail.com>
2015-08-10 00:50:58 +0700

    bugfix, fail to upload the same file during double data entry
    Upload a file to the CRF in initial data entry and mark crf complete,
    now enter data in double data entry and upload the same file.
    
    There will be error saying "The value you specify does not match the
    value from initial data entry".
    
    Problem was caused because the usage of timestamp to ensure file
    uniqueness, where using the hash value of the file content is probably
    the better option.

--- core/src/main/java/org/akaza/openclinica/bean/rule/FileRenamePolicy.java
+++ core/src/main/java/org/akaza/openclinica/bean/rule/FileRenamePolicy.java
@@ -1,9 +1,10 @@
 package org.akaza.openclinica.bean.rule;
 
 import java.io.File;
+import java.io.InputStream;
 
 public interface FileRenamePolicy {
 
-    public File rename(File f);
+    public File rename(File f, InputStream content);
 
 }
