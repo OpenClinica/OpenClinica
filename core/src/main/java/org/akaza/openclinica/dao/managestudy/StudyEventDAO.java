f179d9104a9f3ad980005b48d1a85562d877c759
Steve DiBona <stevedibona@sdib-mbp-osx.local>
2015-10-29 15:11:08 -0400

    OC_6782: Participant chooses when ready to move on

--- core/src/main/java/org/akaza/openclinica/dao/managestudy/StudyEventDAO.java
+++ core/src/main/java/org/akaza/openclinica/dao/managestudy/StudyEventDAO.java
@@ -1058,7 +1058,6 @@ public class StudyEventDAO extends AuditableEntityDAO implements Listener {
         HashMap variables = new HashMap();
         variables.put(Integer.valueOf(1), studySubjectOID);
         variables.put(Integer.valueOf(2), studySubjectOID);
-        variables.put(Integer.valueOf(3), studySubjectOID);
 
         String sql = digester.getQuery("getNextScheduledEvent");
         ArrayList alist = this.select(sql, variables);
