2b8ce40d81efec85c1d60efb477940dcc1b72735
jkeremian <jkeremian@openclinica.com>
2015-12-01 11:25:12 -0500

    OC-6851 Run randomization during data entry

--- core/src/main/java/org/akaza/openclinica/service/crfdata/DynamicsMetadataService.java
+++ core/src/main/java/org/akaza/openclinica/service/crfdata/DynamicsMetadataService.java
@@ -1,6 +1,8 @@
 package org.akaza.openclinica.service.crfdata;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -50,10 +52,28 @@ import org.akaza.openclinica.domain.rule.RuleSetBean;
 import org.akaza.openclinica.domain.rule.action.PropertyBean;
 import org.akaza.openclinica.domain.rule.action.StratificationFactorBean;
 import org.akaza.openclinica.exception.OpenClinicaException;
+import org.akaza.openclinica.service.pmanage.Authorization;
 import org.akaza.openclinica.service.rule.expression.ExpressionService;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.collections.map.HashedMap;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.CommonsClientHttpRequestFactory;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+import org.springframework.security.oauth2.common.json.JSONException;
+import org.springframework.security.oauth2.common.json.JSONObject;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 public class DynamicsMetadataService implements MetadataServiceInterface {
     protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
@@ -72,6 +92,7 @@ public class DynamicsMetadataService implements MetadataServiceInterface {
     private StudyEventDAO studyEventDAO;
     private EventDefinitionCRFDAO eventDefinitionCRFDAO;
     private ExpressionService expressionService;
+    CommonsClientHttpRequestFactory requestFactory = new CommonsClientHttpRequestFactory();
 
     public DynamicsMetadataService(DataSource ds) {
     	// itemsAlreadyShown = new ArrayList<Integer>();
@@ -491,7 +512,12 @@ public class DynamicsMetadataService implements MetadataServiceInterface {
         if(property.getValueExpression() == null) {
             logger.info("There is no ValueExpression for property ="+property.getOid());
              if (stratificationFactorBeans!=null)
-            value=getRandomizationCode(eventCrfBean,stratificationFactorBeans,ruleSet);
+                try {
+                    value=getRandomizationCode(eventCrfBean,stratificationFactorBeans,ruleSet);
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             
         } else {
             String expression =
@@ -986,7 +1012,7 @@ public class DynamicsMetadataService implements MetadataServiceInterface {
         }
     }
 
-    private String getRandomizationCode(EventCRFBean eventCrfBean, List <StratificationFactorBean> stratificationFactorBeans,RuleSetBean ruleSet){
+    private String getRandomizationCode(EventCRFBean eventCrfBean, List <StratificationFactorBean> stratificationFactorBeans,RuleSetBean ruleSet) throws JSONException{
         StudySubjectDAO ssdao = new StudySubjectDAO<>(ds);       
         StudySubjectBean ssBean = (StudySubjectBean) ssdao.findByPK(eventCrfBean.getStudySubjectId());
         String identifier = ssBean.getOid();    // study subject oid
@@ -1002,25 +1028,30 @@ public class DynamicsMetadataService implements MetadataServiceInterface {
             userId=eventCrfBean.getUpdaterId();            
         }
         UserAccountBean uBean = (UserAccountBean) udao.findByPK(userId);
-        String user = uBean.getName();   // event crf updater username
-        String timezone ="America/New_York";
-        int i = 1;
-        String questions="";
-        String exp="";
-        for (StratificationFactorBean stratificationFactorBean :stratificationFactorBeans){
-            i++;      
-            exp =stratificationFactorBean.getStratificationFactor().getValue();
-            if (exp.startsWith("SS.")){
-                questions=questions+"question"+i+"="+getStudySubjectAttrValue(exp, eventCrfBean,ruleSet)+"&";
-            }else{                
-                questions=questions+"question"+i+"="+getExpressionValue(exp, eventCrfBean,ruleSet)+"&";
-            }
-          }
-        
-        String url="identifier="+identifier+"&"+questions+"siteIdentifier="+siteIdentifier+"&user="+user;        
-        System.out.println("The randomize url:  "+ url);
-        
-        return url;
+        String user = uBean.getName();   
+
+      String timezone ="America/New_York";
+      String randomiseUrl= "https://evaluation.sealedenvelope.com/redpill/seti2";
+      String username ="oc";
+      String password ="secret";
+      HttpHeaders headers =createHeaders(username, password);
+      headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+      // retrieve json object if Randomization exist ,otherwise return a null object
+      JSONObject jsonRandObject=retrieveARandomisation(randomiseUrl, ssBean, headers);
+      if (jsonRandObject!=null){
+        return (String) jsonRandObject.get("code"); 
+      }else{
+           // if Site identifier exists ,then update otherwise create new Site identifier
+          addOrUpdateASite(randomiseUrl, sBean, headers, timezone);
+
+             // send for Randomization
+          JSONObject jsonRandomisedObject=randomiseSubject(randomiseUrl, ssBean,sBean ,headers ,user , stratificationFactorBeans,eventCrfBean,ruleSet);
+          if (jsonRandomisedObject!=null)
+              return (String) jsonRandomisedObject.get("code");
+          else
+              return null;
+        }
+
     }
     
     
@@ -1052,7 +1083,13 @@ public class DynamicsMetadataService implements MetadataServiceInterface {
         if (param.equalsIgnoreCase("BIRTHDATE")){
             value = ssBean.getDateOfBirth().toString();
         }else if (param.equalsIgnoreCase("SEX")){
-            value =String.valueOf(ssBean.getGender());
+            if (String.valueOf(ssBean.getGender()).equals("m"))
+                value = "Male";
+            else
+                value="Female";
+                
+ //           value =String.valueOf(ssBean.getGender());
+   
         }else if (param.startsWith(prefix)){
             String gcName= param.substring(21,param.indexOf("\"]"));    
             
@@ -1064,7 +1101,115 @@ public class DynamicsMetadataService implements MetadataServiceInterface {
    return value;
     }
 
+
     
+    private JSONObject retrieveARandomisation(String randomiseUrl,StudySubjectBean studySubject, HttpHeaders headers) throws JSONException{
+       // method : GET
+        randomiseUrl=randomiseUrl+"/api/randomisation?identifier="+studySubject.getId();   // concatenate Study_Siubject_oid
+        RestTemplate rest = new RestTemplate(requestFactory);
+        ResponseEntity<String> response=null;
+        String body=null;
+        JSONObject jsonObject=null;
+        HttpEntity<String> request = new HttpEntity<String>(headers);
+
+        try {
+             response = rest.exchange(randomiseUrl, HttpMethod.GET, request, String.class);
+             body = response.getBody();
+             jsonObject = new JSONObject(body);             
+             
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            logger.error(e.getMessage());
+            logger.error(ExceptionUtils.getStackTrace(e));
+        }
+        return jsonObject;
+        
+    }
+
+    private void addOrUpdateASite(String randomiseUrl,StudyBean studyBean,HttpHeaders headers , String timezone){
+        //mehtod : POST
+        randomiseUrl=randomiseUrl+"/api/sites";
+        RestTemplate rest = new RestTemplate(requestFactory);
+        ResponseEntity<String> response=null;
+        MultiValueMap<String, String> siteMap = new LinkedMultiValueMap<String, String>();
+        siteMap.add("siteIdentifier",studyBean.getOid());
+        siteMap.add("name",studyBean.getName());
+        siteMap.add("timezone",timezone);
+        HttpEntity<MultiValueMap<String, String>> siteRequest = new HttpEntity<MultiValueMap<String, String>>(siteMap, headers);
+
+        try {
+            response = rest.exchange(randomiseUrl, HttpMethod.POST, siteRequest, String.class);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            logger.error(ExceptionUtils.getStackTrace(e));
+        }        
+    }
+
+    private JSONObject randomiseSubject(String randomiseUrl,StudySubjectBean studySubject,StudyBean studyBean, HttpHeaders headers , String user ,List<StratificationFactorBean> stratificationFactorBeans , EventCRFBean eventCrfBean , RuleSetBean ruleSet){
+        // method : POST
+        int i=1;
+        String exp="";
+        String questions="";
+ 
+        randomiseUrl=randomiseUrl+"/api/randomise";   
+        RestTemplate rest = new RestTemplate(requestFactory);
+        ResponseEntity<String> response=null;
+        MultiValueMap<String, String> subjectMap = new LinkedMultiValueMap<String, String>();
+        subjectMap.add("identifier",String.valueOf(studySubject.getId()));
+        subjectMap.add("siteIdentifier",studyBean.getOid());
+        subjectMap.add("user",user);
+        for (StratificationFactorBean stratificationFactorBean :stratificationFactorBeans){
+            i++;      
+            exp =stratificationFactorBean.getStratificationFactor().getValue();
+            if (exp.startsWith("SS.")){
+                subjectMap.add("question"+i,getStudySubjectAttrValue(exp, eventCrfBean,ruleSet));
+
+            }else{                
+               String output =getExpressionValue(exp, eventCrfBean,ruleSet);
+                if (output.equals("1")){
+                   output="<60";
+                } else if(output.equals("2")){
+                    output="60-100";
+                }else if(output.equals("3")){
+                    output=">100";
+               }
+                subjectMap.add("question"+i,output);
+            }
+          }
+
+
+        String body=null;
+        JSONObject jsonObject=null;
+      HttpEntity<MultiValueMap<String, String>> subjectRequest = new HttpEntity<MultiValueMap<String, String>>(subjectMap, headers);
+
+        try {
+            response = rest.exchange(randomiseUrl, HttpMethod.POST, subjectRequest, String.class);
+            body = response.getBody();
+            jsonObject = new JSONObject(body);             
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            logger.error(e.getMessage());
+            System.out.println(ExceptionUtils.getStackTrace(e));
+            logger.error(ExceptionUtils.getStackTrace(e));
+        }        
+        
+        return jsonObject;
+    }
+
+    
+    
+    HttpHeaders createHeaders( final String username, final String password ){
+        return new HttpHeaders(){
+           {
+              String auth = username + ":" + password;
+              byte[] encodedAuth = Base64.encodeBase64( 
+                 auth.getBytes(Charset.forName("US-ASCII")) );
+              String authHeader = "Basic " + new String( encodedAuth );
+              set( "Authorization", authHeader );
+           }
+        };
+     }
     
     
     
