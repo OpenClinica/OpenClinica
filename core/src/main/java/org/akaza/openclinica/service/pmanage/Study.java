49dae2e394d45fd0f521ac145a7df5936fb5c5f4
jkeremian <jkeremian@openclinica.com>
2016-01-04 16:32:18 -0500

    OC-6903 DEVBUG: Requesting Access to Participate

--- core/src/main/java/org/akaza/openclinica/service/pmanage/Study.java
+++ core/src/main/java/org/akaza/openclinica/service/pmanage/Study.java
@@ -4,67 +4,58 @@ import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Study {
-    private String instanceUrl = null;
-    private String studyOid = null;
-    private String host = null;
-    private Organization organization = null;
-    private String studyName = null;
-    private String OpenClinicaVersion = null;
-    private String logoOriginal = null;
 
+    private long id;
+    private String instanceUrl;
+    private String studyOid;
+    private Organization organization;
+    private String host;
+    private String OpenClinicaVersion;
+    private String studyName;
+    
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
     public String getInstanceUrl() {
         return instanceUrl;
     }
-
     public void setInstanceUrl(String instanceUrl) {
         this.instanceUrl = instanceUrl;
     }
-
     public String getStudyOid() {
         return studyOid;
     }
-
     public void setStudyOid(String studyOid) {
         this.studyOid = studyOid;
     }
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
     public Organization getOrganization() {
         return organization;
     }
-
     public void setOrganization(Organization organization) {
         this.organization = organization;
     }
-
+    public String getHost() {
+        return host;
+    }
+    public void setHost(String host) {
+        this.host = host;
+    }
+    public String getOpenClinicaVersion() {
+        return OpenClinicaVersion;
+    }
+    public void setOpenClinicaVersion(String openClinicaVersion) {
+        OpenClinicaVersion = openClinicaVersion;
+    }
     public String getStudyName() {
         return studyName;
     }
-
     public void setStudyName(String studyName) {
         this.studyName = studyName;
     }
 
-    public String getOpenClinicaVersion() {
-        return OpenClinicaVersion;
+    
     }
-
-    public void setOpenClinicaVersion(String OpenClinicaVersion) {
-        this.OpenClinicaVersion = OpenClinicaVersion;
-    }
-
-    public String getLogoOriginal() {
-        return logoOriginal;
-    }
-
-    public void setLogoOriginal(String logoOriginal) {
-        this.logoOriginal = logoOriginal;
-    }
-}
