de25db7c09d2b54f7565af4420e29b0f748a4e15
Steve DiBona <sdibona@openclinica.com>
2016-01-05 10:47:26 -0500

    OC-6061: CRF locked error message

--- web/src/main/java/org/akaza/openclinica/control/core/CoreSecureController.java
+++ web/src/main/java/org/akaza/openclinica/control/core/CoreSecureController.java
@@ -297,13 +297,8 @@ public abstract class CoreSecureController extends HttpServlet {
             UserAccountBean ub = (UserAccountBean) req.getSession().getAttribute(USER_BEAN_NAME);
 
             if (eventCrf != null && crfLocker.isLocked(eventCrf.getId())) {
-            	if (ub != null && ub.getId() == crfLocker.getLockOwner(eventCrf.getId())) {
-            	System.out.println("Unlocking crf " + eventCrf.getId() + " by user " + ub.getId() + " in CoreSecureController.unlockCRFOnError");
-                crfLocker.unlock(eventCrf.getId());
-            	} else if (ub == null) {
-                	System.out.println("Unlocking crf " + eventCrf.getId() + " by user " + "???" + " in CoreSecureController.unlockCRFOnError");
-                    crfLocker.unlock(eventCrf.getId());
-            	}
+                if (ub != null && ub.getId() == crfLocker.getLockOwner(eventCrf.getId())) crfLocker.unlock(eventCrf.getId());
+                else if (ub == null) crfLocker.unlock(eventCrf.getId());
             }
         }
     }
@@ -979,6 +974,7 @@ public abstract class CoreSecureController extends HttpServlet {
 
     public void unlockCRFsForUser(int userId) {
         crfLocker.unlockAllForUser(userId);
+        
     }
 
 
