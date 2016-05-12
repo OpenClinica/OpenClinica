d73b439e8d9429c27f6cce0c6822e4b73e72fc1f
jacobrousseau <j.rousseau@vumc.nl>
2016-05-12 13:39:45 +0200

    Addition of the modification to the WS to retrieve more event information

--- core/src/main/java/org/akaza/openclinica/service/EventServiceInterface.java
+++ core/src/main/java/org/akaza/openclinica/service/EventServiceInterface.java
@@ -1,8 +1,10 @@
 package org.akaza.openclinica.service;
 
 import org.akaza.openclinica.bean.login.UserAccountBean;
+import org.akaza.openclinica.bean.managestudy.StudyEventBean;
 import org.akaza.openclinica.exception.OpenClinicaSystemException;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
@@ -10,5 +12,8 @@ public interface EventServiceInterface {
 
     public HashMap<String, String> scheduleEvent(UserAccountBean user, Date startDateTime, Date endDateTime, String location, String studyUniqueId,
             String siteUniqueId, String eventDefinitionOID, String studySubjectId) throws OpenClinicaSystemException;
+
+    public ArrayList<StudyEventBean> retrieveEventInformation(UserAccountBean user, String studyUniqueId,
+                                                              String siteUniqueId, String eventDefinitionOID, String studySubjectId) throws OpenClinicaSystemException;
 
 }
--- ws/src/main/java/org/akaza/openclinica/ws/EventEndpoint.java
+++ ws/src/main/java/org/akaza/openclinica/ws/EventEndpoint.java
@@ -8,6 +8,7 @@
 package org.akaza.openclinica.ws;
 
 import org.akaza.openclinica.bean.login.UserAccountBean;
+import org.akaza.openclinica.bean.managestudy.StudyEventBean;
 import org.akaza.openclinica.dao.login.UserAccountDAO;
 import org.akaza.openclinica.exception.OpenClinicaSystemException;
 import org.akaza.openclinica.i18n.util.ResourceBundleProvider;
@@ -35,6 +36,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 
 import javax.sql.DataSource;
@@ -62,8 +64,9 @@ public class EventEndpoint {
     /**
      * Constructor
      * 
-     * @param subjectService
-     * @param cctsService
+     * @param eventService
+     * @param dataSource
+     * @param messages
      */
     public EventEndpoint(EventServiceInterface eventService, DataSource dataSource, MessageSource messages) {
         this.eventService = eventService;
@@ -87,7 +90,7 @@ public class EventEndpoint {
 
         DataBinder dataBinder = new DataBinder((studyEventTransferBean));
         Errors errors = dataBinder.getBindingResult();
-        StudyEventTransferValidator studyEventTransferValidator = new StudyEventTransferValidator(dataSource);
+        StudyEventTransferValidator studyEventTransferValidator = new StudyEventTransferValidator(dataSource, StudyEventTransferValidator.ValidationMode.SCHEDULE_EVENT);
         studyEventTransferValidator.validate((studyEventTransferBean), errors);
 
         if (!errors.hasErrors()) {
@@ -101,11 +104,88 @@ public class EventEndpoint {
                 return new DOMSource(mapSuccessConfirmation(h));
             } catch (OpenClinicaSystemException ose) {
                 errors.reject("eventEndpoint.cannot_schedule", "Cannot schedule an event for this Subject.");
+
                 return new DOMSource(mapFailConfirmation(errors));
             }
         } else {
             return new DOMSource(mapFailConfirmation(errors));
         }
+    }
+
+    @PayloadRoot(localPart = "eventInformationRequest", namespace = NAMESPACE_URI_V1)
+    public Source eventInformation(@XPathParam("//e:eventStatus") NodeList eventInformationRequest,
+                                    @XPathParam("//e:study") String study,
+                                    @XPathParam("//e:subjectLabel") String studySubjectLabel,
+                                    @XPathParam("//e:eventDefinitionOID") String eventDefinitionOID) throws Exception {
+        ResourceBundleProvider.updateLocale(new Locale("en_US"));
+        Element eventInformationRequestElement = (Element) eventInformationRequest.item(0);
+
+        StudyEventTransferBean studyEventTransferBean = unMarshallToEventTransferForEventStatus(eventInformationRequestElement);
+        DataBinder dataBinder = new DataBinder(studyEventTransferBean);
+        Errors errors = dataBinder.getBindingResult();
+        StudyEventTransferValidator studyEventTransferValidator = new StudyEventTransferValidator(dataSource, StudyEventTransferValidator.ValidationMode.EVENT_INFORMATION);
+        studyEventTransferValidator.validate(studyEventTransferBean, errors);
+
+        if (!errors.hasErrors()) {
+
+            List<StudyEventBean> studyEventBeanList = getEventService().retrieveEventInformation(studyEventTransferBean.getUser(),
+                    studyEventTransferBean.getStudyUniqueId(),
+                    studyEventTransferBean.getSiteUniqueId(),
+                    studyEventTransferBean.getEventDefinitionOID(),
+                    studyEventTransferBean.getSubjectLabel());
+            return new DOMSource(mapSuccessEventInfomationResponse(studyEventBeanList, eventDefinitionOID, studySubjectLabel));
+        } else {
+            return new DOMSource(mapFailConfirmation(errors));
+        }
+    }
+
+    /**
+     * Create a successful response for the eventInformationRequest
+     * @param studyEventBeanList the list of events of the subject
+     * @return an XML element
+     * @throws Exception in case of an error
+     */
+    private Element mapSuccessEventInfomationResponse(List<StudyEventBean> studyEventBeanList, String eventDefinitionOID, String studySubjectOID) throws Exception {
+        DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+        Document document = docBuilder.newDocument();
+
+        Element responseElement = document.createElementNS(NAMESPACE_URI_V1, "eventInformationResponse");
+        Element resultElement = document.createElementNS(NAMESPACE_URI_V1, "result");
+        Element eventDefinitionOIDElement = document.createElementNS(NAMESPACE_URI_V1, "eventDefinitionOID");
+        Element studySubjectOIDElement = document.createElementNS(NAMESPACE_URI_V1, "studySubjectOID");
+
+        resultElement.setTextContent(messages.getMessage("eventEndpoint.success", null, "Success", locale));
+        eventDefinitionOIDElement.setTextContent(eventDefinitionOID);
+        studySubjectOIDElement.setTextContent(studySubjectOID);
+
+
+        Element eventListElement = document.createElementNS(NAMESPACE_URI_V1, "events");
+        responseElement.appendChild(eventListElement);
+
+        for (StudyEventBean studyEventBean : studyEventBeanList) {
+            eventListElement.appendChild(createStudyEventInformationElement(document, studyEventBean));
+        }
+        return responseElement;
+    }
+
+
+    private Element createStudyEventInformationElement(Document document, StudyEventBean studyEventBean) {
+        Element eventElement = document.createElementNS(NAMESPACE_URI_V1, "eventStatus");
+
+        Element element = document.createElementNS(NAMESPACE_URI_V1, "status");
+        element.setTextContent(studyEventBean.getSubjectEventStatus().getName());
+        eventElement.appendChild(element);
+
+        element = document.createElementNS(NAMESPACE_URI_V1, "repeatNumber");
+        element.setTextContent(studyEventBean.getRepeatingNum() + "");
+        eventElement.appendChild(element);
+
+        element = document.createElementNS(NAMESPACE_URI_V1, "sampleOrdinal");
+        element.setTextContent(studyEventBean.getSampleOrdinal() + "");
+        eventElement.appendChild(element);
+
+        return eventElement;
     }
 
     /**
@@ -140,7 +220,7 @@ public class EventEndpoint {
     /**
      * Process createEvent request by creating StudyEventTransferBean from received payload.
      * 
-     * @param subjectElement
+     * @param eventElement
      * @return SubjectTransferBean
      * @throws ParseException
      */
@@ -179,11 +259,36 @@ public class EventEndpoint {
                 endDate, endTime), getUserAccount());
     return studyEventTransferBean;
 }
+
+
+    private StudyEventTransferBean unMarshallToEventTransferForEventStatus(Element eventElement)
+            throws ParseException {
+
+
+
+        Element eventDefinitionOidElement = DomUtils.getChildElementByTagName(eventElement, "eventDefinitionOID");
+        Element subjectRefElement = DomUtils.getChildElementByTagName(eventElement, "subject");
+        Element labelElement = DomUtils.getChildElementByTagName(subjectRefElement, "uniqueIdentifier");
+
+        Element studyRefElement = DomUtils.getChildElementByTagName(eventElement, "studyRef");
+        Element studyIdentifierElement = DomUtils.getChildElementByTagName(studyRefElement, "identifier");
+        Element siteRef = DomUtils.getChildElementByTagName(studyRefElement, "siteRef");
+        Element siteIdentifierElement = siteRef == null ? null : DomUtils.getChildElementByTagName(siteRef, "identifier");
+
+        String studySubjectId = DomUtils.getTextValue(labelElement).trim();
+        String eventDefinitionOID = DomUtils.getTextValue(eventDefinitionOidElement).trim();
+        String studyIdentifier = DomUtils.getTextValue(studyIdentifierElement).trim();
+        String siteIdentifier = siteIdentifierElement == null ? null : DomUtils.getTextValue(siteIdentifierElement).trim();
+
+        StudyEventTransferBean studyEventTransferBean =
+                new StudyEventTransferBean(studySubjectId, studyIdentifier, siteIdentifier, eventDefinitionOID, null, null, null, getUserAccount());
+        return studyEventTransferBean;
+    }
     
     /**
      * Create Error Response
      * 
-     * @param confirmation
+     * @param errors
      * @return
      * @throws Exception
      */
