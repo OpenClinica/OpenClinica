d4accd74c70ab17c85ff8e3ab0b0735e84b91f0b
jkeremian <jkeremian@openclinica.com>
2016-06-01 15:31:04 -0400

    as

--- core/src/main/java/org/akaza/openclinica/dao/core/CoreResources.java
+++ core/src/main/java/org/akaza/openclinica/dao/core/CoreResources.java
@@ -430,7 +430,8 @@ public class CoreResources implements ResourceLoaderAware {
         String schema =(DATAINFO.getProperty("schema").trim().equals("") ? "public"  : DATAINFO.getProperty("schema").trim());                
         String url = null, driver = null, hibernateDialect = null;
         if (database.equalsIgnoreCase("postgres")) {
-            url = "jdbc:postgresql:" + "//" + DATAINFO.getProperty("dbHost") + ":" + DATAINFO.getProperty("dbPort") + "/" + DATAINFO.getProperty("db");
+            url = "jdbc:postgresql:" + "//" + DATAINFO.getProperty("dbHost") + ":" + DATAINFO.getProperty("dbPort") + "/" + DATAINFO.getProperty("db")+"?currentSchema="+ schema
+;
             driver = "org.postgresql.Driver";
             hibernateDialect = "org.hibernate.dialect.PostgreSQLDialect";
         } else if (database.equalsIgnoreCase("oracle")) {
