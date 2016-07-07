fa7d55dd8c95e33d22b52d201438d1bed15a7e7e
Yogi Shridhare <yshridhare@openclinica.com>
2016-07-07 17:43:32 -0400

    Debug

--- web/src/main/java/org/akaza/openclinica/controller/RestrictedController.java
+++ web/src/main/java/org/akaza/openclinica/controller/RestrictedController.java
@@ -22,13 +22,13 @@ public class RestrictedController {
         System.out.println("****in SSO restricted controller");
 
         Account account = AccountResolver.INSTANCE.getAccount(request);
-        System.out.println("****Account=" + account);
+        //System.out.println("****Account=" + account);
         /*
         if (account == null) {
             return "redirect:/pages/login";
         }
         */
-        logger.info("^^^^^After in SSO restricted controller");
+        logger.info("After in SSO restricted controller");
         return "redirect:/MainMenu";
     }
 }
