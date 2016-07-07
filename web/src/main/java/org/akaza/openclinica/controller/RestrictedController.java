1d59b5bfdb45d3bf8d93960ae1ede588ac1b8841
Yogi Shridhare <yshridhare@openclinica.com>
2016-07-07 16:17:50 -0400

    Allowing for RestrictedController

--- web/src/main/java/org/akaza/openclinica/controller/RestrictedController.java
+++ web/src/main/java/org/akaza/openclinica/controller/RestrictedController.java
@@ -3,6 +3,8 @@ package org.akaza.openclinica.controller;
 import com.stormpath.sdk.account.Account;
 import com.stormpath.sdk.application.Application;
 import com.stormpath.sdk.servlet.account.AccountResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -13,17 +15,18 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RestrictedController {
-
-
+    protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
 
     @RequestMapping("/restricted/secret")
     public String secret(HttpServletRequest request, Model model) {
+        System.out.println("****in SSO restricted controller");
+
         Account account = AccountResolver.INSTANCE.getAccount(request);
 
         if (account == null) {
             return "redirect:/login";
         }
-        System.out.println("****in SSO restricted controller");
+        logger.info("****in SSO restricted controller");
         return "restricted/secret";
     }
 }
