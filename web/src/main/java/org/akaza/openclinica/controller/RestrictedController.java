e6e98bfd977e61c4789b24604d63868df88fe697
Yogi Shridhare <yshridhare@openclinica.com>
2016-07-08 13:05:01 -0400

    Adding stormpath hooks

--- web/src/main/java/org/akaza/openclinica/controller/RestrictedController.java
+++ web/src/main/java/org/akaza/openclinica/controller/RestrictedController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import java.util.Enumeration;
 
 @Controller
 @EnableStormpath
+@PropertySource("classpath:application.properties")
 public class RestrictedController {
     protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
     @Autowired
