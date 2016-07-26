55724f7ccb77783684a42e474982b0b244bc37ab
Steve DiBona <sdibona@openclinica.com>
2016-07-26 14:26:58 -0400

    OC-7300: Support distributed http sessions for use on Heroku.  Updating distributed session configuration.

--- core/src/main/java/org/akaza/openclinica/config/DistributedSessionFilterInitializer.java
+++ core/src/main/java/org/akaza/openclinica/config/DistributedSessionFilterInitializer.java
@@ -5,14 +5,18 @@ import javax.servlet.ServletException;
 
 import org.apache.commons.lang.ArrayUtils;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+import org.springframework.util.StringUtils;
 
 public class DistributedSessionFilterInitializer extends AbstractHttpSessionApplicationInitializer{
+
+    public static final String SPRING_PROFILES_PROPERTY_NAME = "spring.profiles.active";
+
     public void onStartup(ServletContext servletContext) throws ServletException {
         System.out.println("Executing DistributedSessionFilterInitializer");
-        String profiles = System.getProperty("spring.profiles.active");
-        if (profiles != null && !profiles.equals("")) {
+        String profiles = System.getProperty(SPRING_PROFILES_PROPERTY_NAME);
+        if (!StringUtils.isEmpty(profiles)) {
             String[] profileArray = profiles.split(",");
-            if (ArrayUtils.contains(profileArray, "heroku")) {
+            if (ArrayUtils.contains(profileArray, DistributedSessionConfig.DISTRIBUTED_SESSION_SPRING_PROFILE)) {
                 System.out.println("Found heroku profile.  Creating Filter.");
                 super.onStartup(servletContext);
             } else System.out.println("Did not find heroku profile.  Skipping filter.");
