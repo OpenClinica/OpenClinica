20001dfcc17bd406dc3d4d561d8fc9e8b38f09f6
Yogi Shridhare <yshridhare@openclinica.com>
2016-07-28 15:52:31 -0400

    Maven module for Stormpath. new profile localStormpath for locally working on Stormpath. The "heroku" profile includes heroku and stormpath. "local" profile excludes both.

--- stormpath/src/main/java/org/akaza/openclinica/controller/StormpathConfig.java
+++ stormpath/src/main/java/org/akaza/openclinica/controller/StormpathConfig.java
@@ -1,11 +1,8 @@
 package org.akaza.openclinica.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 /**
