f32cfd4b917eabbe78a8225fa1442337d41804b8
Yogi Shridhare <yshridhare@openclinica.com>
2017-07-25 14:30:09 -0400

    Change study status in the public study as well.

--- web/src/main/java/org/akaza/openclinica/controller/StudyModuleController.java
+++ web/src/main/java/org/akaza/openclinica/controller/StudyModuleController.java
@@ -503,16 +503,22 @@ public class StudyModuleController {
         } else {
             currentStudy.setOldStatus(currentStudy.getStatus());
             currentStudy.setStatus(Status.get(studyModuleStatus.getStudyStatus()));
-            if (currentStudy.getParentStudyId() > 0) {
-                studyDao.updateStudyStatus(currentStudy);
-            } else {
-                studyDao.updateStudyStatus(currentStudy);
-            }
-
+            studyDao.updateStudyStatus(currentStudy);
             ArrayList siteList = (ArrayList) studyDao.findAllByParent(currentStudy.getId());
             if (siteList.size() > 0) {
                 studyDao.updateSitesStatus(currentStudy);
             }
+            String currentSchema = CoreResources.getRequestSchema(request);
+            CoreResources.setRequestSchema("public");
+            StudyBean publicStudy = studyDao.findByOid(currentStudy.getOid());
+            publicStudy.setOldStatus(currentStudy.getStatus());
+            publicStudy.setStatus(Status.get(studyModuleStatus.getStudyStatus()));
+            studyDao.updateStudyStatus(publicStudy);
+            ArrayList publicSiteList = (ArrayList) studyDao.findAllByParent(publicStudy.getId());
+            if (publicSiteList.size() > 0) {
+                studyDao.updateSitesStatus(publicStudy);
+            }
+            CoreResources.setRequestSchema(currentSchema);
         }
         return "redirect:studymodule";
     }
