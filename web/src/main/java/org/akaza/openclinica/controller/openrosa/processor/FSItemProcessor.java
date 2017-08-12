4d8a2f532ccf7c04ef3f092c24516a84caa09012
joekeremian <joekeremian@172.28.128.1>
2017-08-12 14:57:46 -0400

    OC-8185
    Deleted Repeating Group entries should not appear when the form is
    reopened

--- web/src/main/java/org/akaza/openclinica/controller/openrosa/processor/FSItemProcessor.java
+++ web/src/main/java/org/akaza/openclinica/controller/openrosa/processor/FSItemProcessor.java
@@ -165,6 +165,7 @@ public class FSItemProcessor extends AbstractItemProcessor implements Processor 
             for (ItemGroupMetadata ig : igms) {
                 ItemData existingItemData = itemDataDao.findByItemEventCrfOrdinal(ig.getItem().getItemId(), container.getEventCrf().getEventCrfId(),
                         itemOrdinal);
+                int maxRowCount = itemDataDao.getMaxCountByEventCrfGroup(container.getEventCrf().getEventCrfId(), ig.getItemGroup().getItemGroupId());
 
                 // ItemData existingItemData = lookupFieldItemData(itemGroup, itemOrdinal, container);
                 if (existingItemData != null) {
@@ -180,7 +181,7 @@ public class FSItemProcessor extends AbstractItemProcessor implements Processor 
 
                     // Close discrepancy notes
                     closeItemDiscrepancyNotes(container, existingItemData);
-                } else {
+                } else if (itemOrdinal < maxRowCount) {
                     ItemData newItemData = createItemData(ig.getItem(), "", itemOrdinal, container);
                     newItemData.setDeleted(true);
                     newItemData = itemDataDao.saveOrUpdate(newItemData);
