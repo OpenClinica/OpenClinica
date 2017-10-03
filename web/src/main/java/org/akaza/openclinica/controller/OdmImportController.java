17025bc99637f05203ee93b4e7ebbcefc9381f29
joekeremian <joekeremian@joe-mac-pro.openclinica.com>
2017-10-03 19:11:46 -0400

    OC-8392
    publish error return 400 fix

--- web/src/main/java/org/akaza/openclinica/controller/OdmImportController.java
+++ web/src/main/java/org/akaza/openclinica/controller/OdmImportController.java
@@ -25,7 +25,7 @@ public class OdmImportController {
             odmImportServiceImpl.importOdm(odm, boardId);
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (CustomRuntimeException e) {
-            return new ResponseEntity<>(e.getErrors(), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
         }
 
     }
