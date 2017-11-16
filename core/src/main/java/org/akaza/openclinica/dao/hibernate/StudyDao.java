19dfa0f56b48e92f58d74c28d9b2fb4aadd6191b
joekeremian <joekeremian@joe-mac-pro.openclinica.com>
2017-11-16 11:05:46 -0500

    OC-8498
        Study Definition is updated by failed Publish

--- core/src/main/java/org/akaza/openclinica/dao/hibernate/StudyDao.java
+++ core/src/main/java/org/akaza/openclinica/dao/hibernate/StudyDao.java
@@ -1,16 +1,17 @@
 package org.akaza.openclinica.dao.hibernate;
 
+import java.math.BigInteger;
+
 import org.akaza.openclinica.dao.core.CoreResources;
-import org.akaza.openclinica.domain.datamap.StudyEnvEnum;
 import org.akaza.openclinica.domain.datamap.Study;
+import org.akaza.openclinica.domain.datamap.StudyEnvEnum;
 import org.apache.commons.lang.StringUtils;
 import org.hibernate.query.Query;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigInteger;
 @Transactional
 public class StudyDao extends AbstractDomainDao<Study> {
-	
+
     @Override
     public Class<Study> domainClass() {
         return Study.class;
@@ -19,14 +20,15 @@ public class StudyDao extends AbstractDomainDao<Study> {
     public boolean doesStudyExist(String uniqueId, String schemaName) {
         Query q = getCurrentSession().createNativeQuery("select count(*) from " + schemaName + ".study where unique_identifier='" + uniqueId + "'");
         BigInteger count = (BigInteger) q.getSingleResult();
-        return (count.intValue() == 1) ? true:false;
+        return (count.intValue() == 1) ? true : false;
     }
+
     public Study findByUniqueId(String uniqueId) {
         getSessionFactory().getStatistics().logSummary();
         String query = " from Study do  where do.uniqueIdentifier = :uniqueId";
         Query q = getCurrentSession().createQuery(query);
         q.setParameter("uniqueId", uniqueId);
-        return  (Study) q.uniqueResult();
+        return (Study) q.uniqueResult();
     }
 
     public Study findByOidEnvType(String oid, StudyEnvEnum envType) {
@@ -35,16 +37,15 @@ public class StudyDao extends AbstractDomainDao<Study> {
         Query q = getCurrentSession().createQuery(query);
         q.setParameter("oid", oid);
         q.setParameter("envType", envType);
-        return  (Study) q.uniqueResult();
+        return (Study) q.uniqueResult();
     }
 
     public Study findByStudyEnvUuid(String studyEnvUuid) {
         getSessionFactory().getStatistics().logSummary();
-        String query = " from Study do  where do.studyEnvUuid = :studyEnvUuid "
-                + "or do.studyEnvSiteUuid = :studyEnvUuid";
+        String query = " from Study do  where do.studyEnvUuid = :studyEnvUuid " + "or do.studyEnvSiteUuid = :studyEnvUuid";
         Query q = getCurrentSession().createQuery(query);
         q.setParameter("studyEnvUuid", studyEnvUuid);
-        return  (Study) q.uniqueResult();
+        return (Study) q.uniqueResult();
     }
 
     public Study findPublicStudy(String ocId) {
@@ -56,10 +57,11 @@ public class StudyDao extends AbstractDomainDao<Study> {
         String query = " from Study do  where do.oc_oid = :ocId";
         Query q = getCurrentSession().createQuery(query);
         q.setParameter("ocId", ocId);
-        Study study =   (Study) q.uniqueResult();
+        Study study = (Study) q.uniqueResult();
         CoreResources.setRequestSchema(schema);
         return study;
     }
+
     public Study findPublicStudyById(int studyId) {
         String schema = CoreResources.getRequestSchema();
         if (StringUtils.isEmpty(schema))
@@ -69,10 +71,11 @@ public class StudyDao extends AbstractDomainDao<Study> {
         String query = " from Study do  where do.studyId = :studyId";
         Query q = getCurrentSession().createQuery(query);
         q.setParameter("studyId", studyId);
-        Study study =   (Study) q.uniqueResult();
+        Study study = (Study) q.uniqueResult();
         CoreResources.setRequestSchema(schema);
         return study;
     }
+
     public Study updatePublicStudy(Study study) {
         String schema = CoreResources.getRequestSchema();
         if (StringUtils.isEmpty(schema))
