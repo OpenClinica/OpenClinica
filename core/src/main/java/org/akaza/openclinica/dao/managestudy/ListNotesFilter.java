852a7f38c34be9f69b2ebf27b9ea94ff6b250daf
joekeremian <joekeremian@joe-mac-pro.openclinica.com>
2017-12-26 12:09:29 -0500

    OC-8441-ViewNotes-OC3
    SQL injection

--- core/src/main/java/org/akaza/openclinica/dao/managestudy/ListNotesFilter.java
+++ core/src/main/java/org/akaza/openclinica/dao/managestudy/ListNotesFilter.java
@@ -1,8 +1,12 @@
 package org.akaza.openclinica.dao.managestudy;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import org.akaza.openclinica.i18n.util.ResourceBundleProvider;
 
 public class ListNotesFilter implements CriteriaCommand {
 
@@ -28,62 +32,84 @@ public class ListNotesFilter implements CriteriaCommand {
         filters.add(new Filter(property, value));
     }
 
-    public String execute(String criteria) {
+    public String execute(String criteria, HashMap variables) {
         String theCriteria = "";
         for (Filter filter : filters) {
             if (columnMapping.get(filter.getProperty()) == null) {
                 continue;
             }
-            theCriteria += buildCriteria(criteria, filter.getProperty(), filter.getValue());
+            theCriteria += buildCriteria(criteria, filter.getProperty(), filter.getValue(), variables);
         }
         return theCriteria;
     }
 
-    private String buildCriteria(String criteria, String property, Object value) {
+    @Override
+    public String execute(String criteria) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    private String buildCriteria(String criteria, String property, Object value, HashMap variables) {
         if (value != null) {
-			if (property.equals("studySubject.labelExact")){
+            if (property.equals("studySubject.labelExact")) {
                 criteria = criteria + " and ";
-                criteria = criteria + " UPPER(" + columnMapping.get(property) + ") = UPPER('" + value.toString() + "')" + " ";
-            }else if (property.equals("studySubject.label") || property.equals("discrepancyNoteBean.description") || property.equals("discrepancyNoteBean.user")) {
+                criteria = criteria + " UPPER(" + columnMapping.get(property) + ") = UPPER(?)" + " ";
+                variables.put(variables.size() + 1, String.valueOf(value.toString()));
+            } else if (property.equals("studySubject.label") || property.equals("discrepancyNoteBean.description")
+                    || property.equals("discrepancyNoteBean.user")) {
                 criteria = criteria + " and ";
-                criteria = criteria + " UPPER(" + columnMapping.get(property) + ") like UPPER('%" + value.toString() + "%')" + " ";
+                criteria = criteria + " UPPER(" + columnMapping.get(property) + ") like UPPER( ? )" + " ";
+                variables.put(variables.size() + 1, '%' + value.toString() + '%');
             } else if (property.equals("siteId")) {
                 criteria = criteria + " and ";
-                criteria = criteria + "ss.study_id in ( SELECT study_id FROM study WHERE unique_identifier like '%"+ value.toString() +"%')";
+                criteria = criteria + "ss.study_id in ( SELECT study_id FROM study WHERE unique_identifier like  ? )";
+                variables.put(variables.size() + 1, '%' + value.toString() + '%');
             } else if (property.equals("age")) {
-                if(value.toString().startsWith(">") || value.toString().startsWith("<")
-                        || value.toString().startsWith("=")){
+                if (value.toString().startsWith(">") || value.toString().startsWith("<") || value.toString().startsWith("=")) {
                     criteria = criteria + " and ";
-                    criteria = criteria + " age " + value.toString();
+                    criteria = criteria + " " + columnMapping.get(property) + " " + value.toString().substring(0, 1) + " ?";
+                    variables.put(variables.size() + 1, Integer.valueOf(value.toString().substring(1)));
                 }
             } else if (property.equals("days")) {
-                if(value.toString().startsWith(">") || value.toString().startsWith("<")
-                        || value.toString().startsWith("=")){
+                if (value.toString().startsWith(">") || value.toString().startsWith("<") || value.toString().startsWith("=")) {
                     criteria = criteria + " and ";
-                    criteria = criteria + " days " + value.toString();
+                    criteria = criteria + " " + columnMapping.get(property) + " " + value.toString().substring(0, 1) + " ?";
+                    variables.put(variables.size() + 1, Integer.valueOf(value.toString().substring(1)));
                 }
             } else if ("discrepancyNoteBean.disType".equalsIgnoreCase(property)) {
-                if("31".equals(value.toString())) {
+                if ("31".equals(value.toString())) {
                     criteria = criteria + " and ";
                     criteria = criteria + " (dn.discrepancy_note_type_id = 1 or dn.discrepancy_note_type_id = 3)";
                 } else {
                     criteria = criteria + " and ";
-                    criteria = criteria + " " + columnMapping.get(property) + " = '" + value.toString() + "' ";
+                    criteria = criteria + " " + columnMapping.get(property) + " = ? ";
+                    variables.put(variables.size() + 1, Integer.valueOf(value.toString()));
                 }
             } else if ("discrepancyNoteBean.resolutionStatus".equalsIgnoreCase(property)) {
-                if("21".equals(value.toString())) {
+                if ("21".equals(value.toString())) {
                     criteria = criteria + " and ";
                     criteria = criteria + " (dn.resolution_status_id = 1 or dn.resolution_status_id = 2)";
                 } else {
                     criteria = criteria + " and ";
-                    criteria = criteria + " " + columnMapping.get(property) + " = '" + value.toString() + "' ";
+                    criteria = criteria + " " + columnMapping.get(property) + " = ? ";
+                    variables.put(variables.size() + 1, Integer.valueOf(value.toString()));
                 }
-            } else if ("discrepancyNoteBean.createdDate".equalsIgnoreCase(property) || "discrepancyNoteBean.updatedDate".equalsIgnoreCase(property) ) {                
-                    criteria = criteria + " and ";
-                    criteria = criteria + " " + columnMapping.get(property) + "::timestamp::date = '" + value.toString() + "' ";                
+            } else if ("discrepancyNoteBean.createdDate".equalsIgnoreCase(property) || "discrepancyNoteBean.updatedDate".equalsIgnoreCase(property)) {
+                criteria = criteria + " and ";
+                criteria = criteria + " " + columnMapping.get(property) + "::timestamp::date = ? ";
+                String format = ResourceBundleProvider.getFormatBundle().getString("date_format_string");
+                SimpleDateFormat sdf = new SimpleDateFormat(format);
+                try {
+                    variables.put(variables.size() + 1, sdf.parse(value.toString()));
+                } catch (ParseException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
             } else {
                 criteria = criteria + " and ";
-                criteria = criteria + " " + columnMapping.get(property) + " = '" + value.toString() + "' ";
+                criteria = criteria + " " + columnMapping.get(property) + " = ? ";
+                variables.put(variables.size() + 1, String.valueOf(value.toString()));
             }
         }
         return criteria;
@@ -110,5 +136,5 @@ public class ListNotesFilter implements CriteriaCommand {
     public List<Filter> getFilters() {
         return filters;
     }
-    
+
 }
