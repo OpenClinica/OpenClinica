9eaaf5597ddaccb9abd0b3b35c8daf519a7500d6
joekeremian <joekeremian@10.0.0.128>
2018-02-26 08:18:23 -0500

    OC-8821
    Add new Event CRFs from the View Study Subject Overhaul page

---
+++ web/src/main/java/org/akaza/openclinica/controller/dto/SSOverhaulDTO.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.akaza.openclinica.controller.dto;
+
+/**
+ * @author joekeremian
+ *
+ */
+public class SSOverhaulDTO {
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
