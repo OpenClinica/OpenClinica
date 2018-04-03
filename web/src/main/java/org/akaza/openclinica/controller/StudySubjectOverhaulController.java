a2af22377ce8187a8f80887a245f1b062aadd766
joekeremian <joekeremian@10.0.0.129>
2018-04-03 16:30:17 -0400

    OC-8913
    org.akaza.openclinica.service.rule.RulesPostImportContainerServiceTest

--- web/src/main/java/org/akaza/openclinica/controller/StudySubjectOverhaulController.java
+++ web/src/main/java/org/akaza/openclinica/controller/StudySubjectOverhaulController.java
@@ -48,193 +48,194 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudySubjectOverhaulController {
 
-    @Autowired
-    private StudyEventDao studyEventDao;
+	@Autowired
+	private StudyEventDao studyEventDao;
 
-    @Autowired
-    private StudySubjectDao studySubjectDao;
+	@Autowired
+	private StudySubjectDao studySubjectDao;
 
-    @Autowired
-    private StudyDao studyDao;
+	@Autowired
+	private StudyDao studyDao;
 
-    @Autowired
-    private StudyEventDefinitionDao studyEventDefintionDao;
+	@Autowired
+	private StudyEventDefinitionDao studyEventDefintionDao;
 
-    @Autowired
-    private CrfDao crfDao;
+	@Autowired
+	private CrfDao crfDao;
 
-    @Autowired
-    private EventCrfDao eventCrfDao;
+	@Autowired
+	private EventCrfDao eventCrfDao;
 
-    @Autowired
-    private EventDefinitionCrfDao eventDefinitionCrfDao;
+	@Autowired
+	private EventDefinitionCrfDao eventDefinitionCrfDao;
 
-    @Autowired
-    private UserAccountDao userAccountDao;
+	@Autowired
+	private UserAccountDao userAccountDao;
 
-    @Autowired
-    private PageLayoutDao pageLayoutDao;
+	@Autowired
+	private PageLayoutDao pageLayoutDao;
 
-    protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
-    private final String COMMON = "common";
+	protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
+	private final String COMMON = "common";
 
-    /**
-     * Schedule new event Overhaul and add a form to event
-     * 
-     * @param request
-     * @param studyOid
-     * @param studyEventDefinitionOid
-     * @param crfOid
-     * @param studySubjectOid
-     * @return
-     * @throws IOException
-     * @throws URISyntaxException
-     */
-    @SuppressWarnings("null")
-    @Produces(MediaType.APPLICATION_JSON)
-    @RequestMapping(value = "/api/addAnotherForm", method = RequestMethod.POST)
-    public ResponseEntity<SSOverhaulDTO> scheduleNewEvent(HttpServletRequest request, @RequestParam("studyoid") String studyOid,
-            @RequestParam("studyeventdefinitionoid") String studyEventDefinitionOid, @RequestParam("crfoid") String crfOid,
-            @RequestParam("studysubjectoid") String studySubjectOid) throws IOException, URISyntaxException {
+	/**
+	 * Schedule new event Overhaul and add a form to event
+	 * 
+	 * @param request
+	 * @param studyOid
+	 * @param studyEventDefinitionOid
+	 * @param crfOid
+	 * @param studySubjectOid
+	 * @return
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
+	@SuppressWarnings("null")
+	@Produces(MediaType.APPLICATION_JSON)
+	@RequestMapping(value = "/api/addAnotherForm", method = RequestMethod.POST)
+	public ResponseEntity<SSOverhaulDTO> scheduleNewEvent(HttpServletRequest request, @RequestParam("studyoid") String studyOid,
+			@RequestParam("studyeventdefinitionoid") String studyEventDefinitionOid, @RequestParam("crfoid") String crfOid,
+			@RequestParam("studysubjectoid") String studySubjectOid) throws IOException, URISyntaxException {
 
-        SSOverhaulDTO obj = null;
-        request.setAttribute("requestSchema", "public");
-        HttpSession session = request.getSession();
-        Study publicstudy = studyDao.findByOcOID(studyOid);
-        UserAccountBean ub = (UserAccountBean) session.getAttribute("userBean");
-        if (ub == null) {
-            logger.error("userAccount with username {} is null", ub.getName());
-            return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        UserAccount userAccount = userAccountDao.findById(ub.getId());
-        if (userAccount == null) {
-            logger.error("userAccount with username {} is null", userAccount.getUserName());
-            return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+		SSOverhaulDTO obj = null;
+		request.setAttribute("requestSchema", "public");
+		HttpSession session = request.getSession();
+		Study publicstudy = studyDao.findByOcOID(studyOid);
+		UserAccountBean ub = (UserAccountBean) session.getAttribute("userBean");
+		if (ub == null) {
+			logger.error("userAccount with username {} is null", ub.getName());
+			return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		UserAccount userAccount = userAccountDao.findById(ub.getId());
+		if (userAccount == null) {
+			logger.error("userAccount with username {} is null", userAccount.getUserName());
+			return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 
-        request.setAttribute("requestSchema", publicstudy.getSchemaName());
-        Study study = studyDao.findByOcOID(studyOid);
+		request.setAttribute("requestSchema", publicstudy.getSchemaName());
+		Study study = studyDao.findByOcOID(studyOid);
 
-        if (study == null) {
-            logger.error("Study with Oid {} is null", study.getOc_oid());
-            return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
-        } else if (study.getStudy() == null) {
-            logger.info("the study with Oid {} is a Parent study", study.getOc_oid());
-        } else {
-            logger.info("the study with Oid {} is a Site study", study.getOc_oid());
-        }
+		if (study == null) {
+			logger.error("Study with Oid {} is null", study.getOc_oid());
+			return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+		} else if (study.getStudy() == null) {
+			logger.info("the study with Oid {} is a Parent study", study.getOc_oid());
+		} else {
+			logger.info("the study with Oid {} is a Site study", study.getOc_oid());
+		}
 
-        StudySubject studySubject = studySubjectDao.findByOcOID(studySubjectOid);
-        if (studySubject == null) {
-            logger.error("StudySubject with Oid {} is null", studySubject.getOcOid());
-            return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        StudyEventDefinition studyEventDefinition = studyEventDefintionDao.findByOcOID(studyEventDefinitionOid);
-        if (studyEventDefinition == null) {
-            logger.error("StudyEventDefinition with Oid {} is null", studyEventDefinition.getOc_oid());
-            return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
-        } else if (!studyEventDefinition.getType().equals(COMMON)) {
-            logger.error("StudyEventDefinition with Oid {} is not a Common Type Event", studyEventDefinition.getOc_oid());
-            return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        CrfBean crf = crfDao.findByOcOID(crfOid);
-        if (crf == null) {
-            logger.error("Crf with Oid {} is null", crf.getOcOid());
-            return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+		StudySubject studySubject = studySubjectDao.findByOcOID(studySubjectOid);
+		if (studySubject == null) {
+			logger.error("StudySubject with Oid {} is null", studySubject.getOcOid());
+			return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		StudyEventDefinition studyEventDefinition = studyEventDefintionDao.findByOcOID(studyEventDefinitionOid);
+		if (studyEventDefinition == null) {
+			logger.error("StudyEventDefinition with Oid {} is null", studyEventDefinition.getOc_oid());
+			return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+		} else if (!studyEventDefinition.getType().equals(COMMON)) {
+			logger.error("StudyEventDefinition with Oid {} is not a Common Type Event", studyEventDefinition.getOc_oid());
+			return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		CrfBean crf = crfDao.findByOcOID(crfOid);
+		if (crf == null) {
+			logger.error("Crf with Oid {} is null", crf.getOcOid());
+			return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 
-        EventDefinitionCrf edc = eventDefinitionCrfDao.findByStudyEventDefinitionIdAndCRFIdAndStudyId(studyEventDefinition.getStudyEventDefinitionId(),
-                crf.getCrfId(), study.getStudyId());
-        if (edc == null) {
-            edc = eventDefinitionCrfDao.findByStudyEventDefinitionIdAndCRFIdAndStudyId(studyEventDefinition.getStudyEventDefinitionId(), crf.getCrfId(),
-                    study.getStudy().getStudyId());
-        }
-        if (edc == null) {
-            logger.error("EventDefinitionCrf for StudyEventDefinition Oid {},Crf Oid {} and Study Oid {}is null", studyEventDefinition.getOc_oid(),
-                    crf.getOcOid(), study.getOc_oid());
-            return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        FormLayout formLayout = edc.getFormLayout();
-        if (formLayout == null) {
-            logger.error("FormLayout with Oid {} is null", formLayout.getOcOid());
-            return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+		EventDefinitionCrf edc = eventDefinitionCrfDao.findByStudyEventDefinitionIdAndCRFIdAndStudyId(studyEventDefinition.getStudyEventDefinitionId(),
+				crf.getCrfId(), study.getStudyId());
+		if (edc == null) {
+			edc = eventDefinitionCrfDao.findByStudyEventDefinitionIdAndCRFIdAndStudyId(studyEventDefinition.getStudyEventDefinitionId(), crf.getCrfId(),
+					study.getStudy().getStudyId());
+		}
+		if (edc == null) {
+			logger.error("EventDefinitionCrf for StudyEventDefinition Oid {},Crf Oid {} and Study Oid {}is null", studyEventDefinition.getOc_oid(),
+					crf.getOcOid(), study.getOc_oid());
+			return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		FormLayout formLayout = edc.getFormLayout();
+		if (formLayout == null) {
+			logger.error("FormLayout with Oid {} is null", formLayout.getOcOid());
+			return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 
-        List<StudyEvent> studyEvents = studyEventDao.fetchListByStudyEventDefOID(studyEventDefinitionOid, studySubject.getStudySubjectId());
-        Integer maxOrdinal = null;
-        StudyEvent studyEvent = null;
-        if (studyEvents != null && studyEvents.size() != 0) {
-            maxOrdinal = studyEventDao.findMaxOrdinalByStudySubjectStudyEventDefinition(studySubject.getStudySubjectId(),
-                    studyEventDefinition.getStudyEventDefinitionId());
+		List<StudyEvent> studyEvents = studyEventDao.fetchListByStudyEventDefOID(studyEventDefinitionOid, studySubject.getStudySubjectId());
+		Integer maxOrdinal = null;
+		StudyEvent studyEvent = null;
+		if (studyEvents != null && studyEvents.size() != 0) {
+			maxOrdinal = studyEventDao.findMaxOrdinalByStudySubjectStudyEventDefinition(studySubject.getStudySubjectId(),
+					studyEventDefinition.getStudyEventDefinitionId());
 
-            if (!studyEventDefinition.getRepeating()) {
-                logger.error("StudyEventDefinition with Oid {} is Non Repeating", studyEventDefinition.getOc_oid());
+			if (!studyEventDefinition.getRepeating()) {
+				logger.error("StudyEventDefinition with Oid {} is Non Repeating", studyEventDefinition.getOc_oid());
 
-                for (StudyEvent sEvent : studyEvents) {
-                    List<EventCrf> eventCrfs = (List<EventCrf>) eventCrfDao.findByStudyEventIdStudySubjectIdCrfId(sEvent.getStudyEventId(),
-                            studySubject.getStudySubjectId(), crf.getCrfId());
-                    if (eventCrfs.size() != 0) {
-                        logger.error("EventCrf with StudyEventDefinition Oid {},Crf Oid {} and StudySubjectOid {} already exist in the System",
-                                studyEventDefinition.getOc_oid(), crf.getOcOid(), studySubject.getOcOid());
-                        return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
-                    }
-                }
-            }
-        } else {
-            logger.error("No previous study event found for this studyEventDef Oid {} and subject Oid{}", studyEventDefinition.getOc_oid(),
-                    studySubject.getOcOid());
-            maxOrdinal = new Integer(0);
-        }
-        studyEvent = scheduleNewStudyEvent(studySubject, studyEventDefinition, maxOrdinal, userAccount);
+				for (StudyEvent sEvent : studyEvents) {
+					List<EventCrf> eventCrfs = (List<EventCrf>) eventCrfDao.findByStudyEventIdStudySubjectIdCrfId(sEvent.getStudyEventId(),
+							studySubject.getStudySubjectId(), crf.getCrfId());
+					if (eventCrfs.size() != 0) {
+						logger.error("EventCrf with StudyEventDefinition Oid {},Crf Oid {} and StudySubjectOid {} already exist in the System",
+								studyEventDefinition.getOc_oid(), crf.getOcOid(), studySubject.getOcOid());
+						return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+					}
+				}
+			}
+		} else {
+			logger.error("No previous study event found for this studyEventDef Oid {} and subject Oid{}", studyEventDefinition.getOc_oid(),
+					studySubject.getOcOid());
+			maxOrdinal = new Integer(0);
+		}
+		studyEvent = scheduleNewStudyEvent(studySubject, studyEventDefinition, maxOrdinal, userAccount);
 
-        if (studyEvent == null) {
-            logger.error("StudyEvent with studyEventId {} is null", studyEvent.getStudyEventId());
-            return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+		if (studyEvent == null) {
+			logger.error("StudyEvent with studyEventId {} is null", studyEvent.getStudyEventId());
+			return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 
-        String url = "/EnketoFormServlet?formLayoutId=" + formLayout.getFormLayoutId() + "&studyEventId=" + studyEvent.getStudyEventId()
-                + "&eventCrfId=0&originatingPage=ViewStudySubject%3Fid%3D" + studySubject.getStudySubjectId() + "&mode=edit";
+		String url = "/EnketoFormServlet?formLayoutId=" + formLayout.getFormLayoutId() + "&studyEventId=" + studyEvent.getStudyEventId()
+				+ "&eventCrfId=0&originatingPage=ViewStudySubject%3Fid%3D" + studySubject.getStudySubjectId() + "&mode=edit";
 
-        obj = new SSOverhaulDTO();
-        obj.setUrl(url);
-        return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.OK);
-    }
+		obj = new SSOverhaulDTO();
+		obj.setUrl(url);
+		return new ResponseEntity<SSOverhaulDTO>(obj, org.springframework.http.HttpStatus.OK);
+	}
 
-    /**
-     * populate new study event object and save in db
-     * 
-     * @param studySubject
-     * @param studyEventDefinition
-     * @param maxOrdinal
-     * @param userAccount
-     * @return
-     */
-    private StudyEvent scheduleNewStudyEvent(StudySubject studySubject, StudyEventDefinition studyEventDefinition, Integer maxOrdinal,
-            UserAccount userAccount) {
-        StudyEvent studyEvent = new StudyEvent();
-        studyEvent.setStudyEventDefinition(studyEventDefinition);
-        studyEvent.setSampleOrdinal(maxOrdinal + new Integer(1));
-        studyEvent.setSubjectEventStatusId(SubjectEventStatus.NOT_SCHEDULED.getCode());
-        studyEvent.setStatusId(Status.AVAILABLE.getCode());
-        studyEvent.setStudySubject(studySubject);
-        studyEvent.setDateCreated(new Date());
-        studyEvent.setUserAccount(userAccount);
-        studyEvent.setDateStart(null);
-        studyEvent.setStartTimeFlag(new Boolean(false));
-        studyEvent.setEndTimeFlag(new Boolean(false));
-        studyEvent = studyEventDao.saveOrUpdate(studyEvent);
-        return studyEvent;
+	/**
+	 * populate new study event object and save in db
+	 * 
+	 * @param studySubject
+	 * @param studyEventDefinition
+	 * @param maxOrdinal
+	 * @param userAccount
+	 * @return
+	 */
+	private StudyEvent scheduleNewStudyEvent(StudySubject studySubject, StudyEventDefinition studyEventDefinition, Integer maxOrdinal,
+			UserAccount userAccount) {
+		StudyEvent studyEvent = new StudyEvent();
+		studyEvent.setStudyEventDefinition(studyEventDefinition);
+		studyEvent.setSampleOrdinal(maxOrdinal + new Integer(1));
+		studyEvent.setSubjectEventStatusId(SubjectEventStatus.NOT_SCHEDULED.getCode());
+		studyEvent.setStatusId(Status.AVAILABLE.getCode());
+		studyEvent.setStudySubject(studySubject);
+		studyEvent.setDateCreated(new Date());
+		studyEvent.setUserAccount(userAccount);
+		studyEvent.setDateStart(null);
+		studyEvent.setStartTimeFlag(new Boolean(false));
+		studyEvent.setEndTimeFlag(new Boolean(false));
+		studyEvent = studyEventDao.saveOrUpdate(studyEvent);
+		return studyEvent;
 
-    }
+	}
 
-    @Produces(MediaType.APPLICATION_JSON)
-    @RequestMapping(value = "/api/studies/{studyoid}/pages/{name}", method = RequestMethod.GET)
-    public ResponseEntity<Page> getPageLayout(HttpServletRequest request, @PathVariable("studyoid") String studyOid, @PathVariable("name") String name) {
-        Page page = null;
-        PageLayout pageLayout = pageLayoutDao.findByPageLayoutName(name);
-        if (pageLayout != null) {
-            page = (Page) SerializationUtils.deserialize(pageLayout.getDefinition());
-        }
-        return new ResponseEntity<Page>(page, org.springframework.http.HttpStatus.OK);
-    }
+	@Produces(MediaType.APPLICATION_JSON)
+	@RequestMapping(value = "/api/studies/{studyoid}/pages/{name}", method = RequestMethod.GET)
+	public ResponseEntity<Page> getPageLayout(HttpServletRequest request, @PathVariable("studyoid") String studyOid, @PathVariable("name") String name) {
+		Page page = null;
+		PageLayout pageLayout = pageLayoutDao.findByPageLayoutName(name);
+		if (pageLayout != null) {
+			page = (Page) SerializationUtils.deserialize(pageLayout.getDefinition());
+			logger.info("Page Object retrieved from database with page name: {}", pageLayout.getName());
+		}
+		return new ResponseEntity<Page>(page, org.springframework.http.HttpStatus.OK);
+	}
 }
