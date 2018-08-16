614edd0bf904cb068ffd2d17990022887d658aaa
7KRW-T570-W10\Tao Li <tli@openclinica.com>
2018-08-16 10:44:58 -0400

    chech in one more new file

---
+++ core/src/main/java/org/akaza/openclinica/bean/login/ErrorMessage.java
@@ -0,0 +1,20 @@
+package org.akaza.openclinica.bean.login;
+
+public class ErrorMessage {
+
+	private String code;
+	private String message;
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
