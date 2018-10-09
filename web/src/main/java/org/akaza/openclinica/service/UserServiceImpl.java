8cdf715ccd3577771568939051a544db5b2ee9be
joekeremian <jkeremian@openclinica.com>
2018-10-09 10:36:58 -0400

    OC-9665
    Webservice to create and update participant user profile information

--- web/src/main/java/org/akaza/openclinica/service/UserServiceImpl.java
+++ web/src/main/java/org/akaza/openclinica/service/UserServiceImpl.java
@@ -1,62 +1,27 @@
 package org.akaza.openclinica.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.akaza.openclinica.bean.admin.CRFBean;
-import org.akaza.openclinica.bean.core.DataEntryStage;
-import org.akaza.openclinica.bean.login.UserAccountBean;
-import org.akaza.openclinica.bean.login.UserDTO;
 import org.akaza.openclinica.bean.managestudy.*;
-import org.akaza.openclinica.bean.service.StudyParameterValueBean;
-import org.akaza.openclinica.bean.submit.EventCRFBean;
-import org.akaza.openclinica.bean.submit.FormLayoutBean;
 import org.akaza.openclinica.controller.helper.RestfulServiceHelper;
-import org.akaza.openclinica.dao.admin.CRFDAO;
 import org.akaza.openclinica.dao.core.CoreResources;
 import org.akaza.openclinica.dao.hibernate.EventCrfDao;
 import org.akaza.openclinica.dao.hibernate.StudyDao;
 import org.akaza.openclinica.dao.hibernate.StudyEventDao;
 import org.akaza.openclinica.dao.hibernate.StudySubjectDao;
 import org.akaza.openclinica.dao.managestudy.StudyDAO;
-import org.akaza.openclinica.dao.managestudy.StudyEventDefinitionDAO;
-import org.akaza.openclinica.dao.managestudy.StudySubjectDAO;
-import org.akaza.openclinica.dao.service.StudyParameterValueDAO;
-import org.akaza.openclinica.dao.submit.EventCRFDAO;
-import org.akaza.openclinica.dao.submit.FormLayoutDAO;
-import org.akaza.openclinica.dao.submit.ItemDataDAO;
-import org.akaza.openclinica.domain.Status;
 import org.akaza.openclinica.domain.datamap.*;
-import org.akaza.openclinica.domain.user.UserAccount;
-import org.akaza.openclinica.patterns.ocobserver.StudyEventChangeDetails;
-import org.akaza.openclinica.patterns.ocobserver.StudyEventContainer;
-import org.akaza.openclinica.web.pform.PFormCache;
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.apache.commons.lang.exception.ExceptionUtils;
-import org.cdisc.ns.odm.v130.*;
-import org.openclinica.ns.odm_ext_v130.v31.OCodmComplexTypeDefinitionLink;
-import org.openclinica.ns.odm_ext_v130.v31.OCodmComplexTypeDefinitionLinks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.*;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
-import springfox.documentation.spring.web.json.Json;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.POST;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.Comparator;
 
 /**
  * This Service class is used with View Study Subject Page
@@ -117,16 +82,16 @@ public class UserServiceImpl implements UserService {
 
         if (studySubject != null) {
             ocUserDTO = buildOCUserDTO(ssid, participantDTO);
-            if (studySubject.getParticipateUuid() == null) {
+            if (studySubject.getUserUuid() == null) {
                 // create participant user Account   POST
                 object = createOrUpdateParticipantAccount(request, ocUserDTO, HttpMethod.POST);
                 if (object instanceof OCUserDTO && object != null) {
-                    studySubject.setParticipateUuid(((OCUserDTO) object).getUuid());
+                    studySubject.setUserUuid(((OCUserDTO) object).getUuid());
                     studySubjectDao.saveOrUpdate(studySubject);
                 }
             } else {
                 // update participant user Account  PUT
-                ocUserDTO.setUuid(studySubject.getParticipateUuid());
+                ocUserDTO.setUuid(studySubject.getUserUuid());
                 // Get participant
                 Object getParticipantObject = getParticipantAccount(request, ocUserDTO, HttpMethod.GET);
                 if (getParticipantObject instanceof OCUserDTO) {
