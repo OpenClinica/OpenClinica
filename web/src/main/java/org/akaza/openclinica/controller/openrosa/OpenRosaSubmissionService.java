cacf5aa430308d4296a66364ca1feac4cf7bdaaa
joekeremian <jkeremian@openclinica.com>
2018-11-16 15:19:50 -0500

    OC-9868
    Owner Id is not getting captured in the database when user adds a query to a field and then completes the form

--- web/src/main/java/org/akaza/openclinica/controller/openrosa/OpenRosaSubmissionService.java
+++ web/src/main/java/org/akaza/openclinica/controller/openrosa/OpenRosaSubmissionService.java
@@ -9,6 +9,7 @@ import org.akaza.openclinica.dao.hibernate.CrfVersionDao;
 import org.akaza.openclinica.dao.hibernate.StudyDao;
 import org.akaza.openclinica.domain.datamap.CrfVersion;
 import org.akaza.openclinica.domain.datamap.Study;
+import org.akaza.openclinica.domain.user.UserAccount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,27 +29,29 @@ public class OpenRosaSubmissionService {
 
     @Transactional
     public void processRequest(Study study, HashMap<String, String> subjectContext, String requestBody, Errors errors, Locale locale,
-            ArrayList<HashMap> listOfUploadFilePaths, SubmissionContainer.FieldRequestTypeEnum requestType) throws Exception {
+            ArrayList<HashMap> listOfUploadFilePaths, SubmissionContainer.FieldRequestTypeEnum requestType,UserAccount userAccount) throws Exception {
         // Execute save as Hibernate transaction to avoid partial imports
         CrfVersion crfVersion = crfVersionDao.findByOcOID(subjectContext.get("crfVersionOID"));
         String requestPayload = parseSubmission(requestBody, crfVersion);
-        runAsTransaction(study, requestPayload, subjectContext, errors, locale, listOfUploadFilePaths, requestType);
+        runAsTransaction(study, requestPayload, subjectContext, errors, locale, listOfUploadFilePaths, requestType,userAccount);
     }
 
     @Transactional
     public void processFieldSubmissionRequest(Study study, HashMap<String, String> subjectContext, String instanceId, String requestBody, Errors errors,
-            Locale locale, ArrayList<HashMap> listOfUploadFilePaths, SubmissionContainer.FieldRequestTypeEnum requestType) throws Exception {
+            Locale locale, ArrayList<HashMap> listOfUploadFilePaths, SubmissionContainer.FieldRequestTypeEnum requestType,UserAccount userAccount) throws Exception {
         SubmissionContainer container = new SubmissionContainer(study, requestBody, subjectContext, errors, locale, listOfUploadFilePaths, requestType,
                 instanceId);
+        container.setUser(userAccount);
         container.setProcessorEnum(checkInitialInstanceIdSubmission(requestBody));
         container.setFieldSubmissionFlag(true);
         submissionProcessorChain.processSubmission(container);
     }
 
     private void runAsTransaction(Study study, String requestBody, HashMap<String, String> subjectContext, Errors errors, Locale locale,
-            ArrayList<HashMap> listOfUploadFilePaths, SubmissionContainer.FieldRequestTypeEnum requestType) throws Exception {
+                                  ArrayList<HashMap> listOfUploadFilePaths, SubmissionContainer.FieldRequestTypeEnum requestType, UserAccount userAccount) throws Exception {
 
         SubmissionContainer container = new SubmissionContainer(study, requestBody, subjectContext, errors, locale, listOfUploadFilePaths, requestType, null);
+        container.setUser(userAccount);
         container.setProcessorEnum(ProcessorEnum.SUBMISSION_PROCESSOR);
         container.setFieldSubmissionFlag(false);
         submissionProcessorChain.processSubmission(container);
