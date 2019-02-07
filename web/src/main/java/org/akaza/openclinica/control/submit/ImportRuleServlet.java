873225b126b26afbdc1e898d2f227f3913fdf031
theywa <theywaquarius@gmail.com>
2019-02-07 21:29:12 +0800

    OC-9409 Some user roles are unable to download the Data Import template

--- web/src/main/java/org/akaza/openclinica/control/submit/ImportRuleServlet.java
+++ web/src/main/java/org/akaza/openclinica/control/submit/ImportRuleServlet.java
@@ -235,7 +235,7 @@ public class ImportRuleServlet extends SecureController {
             return;
         }
         Role r = currentRole.getRole();
-        if (r.equals(Role.STUDYDIRECTOR) || r.equals(Role.COORDINATOR)) {
+        if (r.equals(Role.STUDYDIRECTOR) || r.equals(Role.COORDINATOR) || r.equals(Role.STUDY_RESEARCHASSISTANT) || r.equals(Role.STUDY_INVESTIGATOR) || r.equals(Role.INVESTIGATOR)) {
             return;
         }
         addPageMessage(respage.getString("no_have_correct_privilege_current_study") + respage.getString("change_study_contact_sysadmin"));
