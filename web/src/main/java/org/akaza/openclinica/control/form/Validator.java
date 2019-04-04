5be8a70d568a80c7ae10d2e6343b33d943de8352
Tao Li <tli@openclinica.com>
2019-04-04 09:42:03 -0400

    Oc-10549:Invalid value imported into decimal field -code changes based on final requirements deccision.

--- web/src/main/java/org/akaza/openclinica/control/form/Validator.java
+++ web/src/main/java/org/akaza/openclinica/control/form/Validator.java
@@ -1222,6 +1222,10 @@ break;
             return true;
         }
         try {
+        	//OC-10549
+        	if(fieldValue.toLowerCase().endsWith("d") || fieldValue.toLowerCase().endsWith("f") || fieldValue.toLowerCase().indexOf("e") > 0) {
+        		return false;
+        	}
             float f = Float.parseFloat(fieldValue);
         } catch (Exception e) {
             return false;
