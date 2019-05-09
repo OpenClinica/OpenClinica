e3d38d531c026a7fe1f75761affc6288add22c20
Christian Hänsel <chae.reliatec@googlemail.com>
2019-05-09 14:59:12 +0200

    since the library updates the export didn't worked and this fixes the errors during the export

--- core/src/main/java/org/akaza/openclinica/domain/rule/RuleSetRuleBean.java
+++ core/src/main/java/org/akaza/openclinica/domain/rule/RuleSetRuleBean.java
@@ -44,7 +44,6 @@ import org.hibernate.annotations.Parameter;
 @Entity
 @Table(name = "rule_set_rule")
 @GenericGenerator(name = "id-generator", strategy = "native", parameters = { @Parameter(name = "sequence_name", value = "rule_set_rule_id_seq") })
-@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class RuleSetRuleBean extends AbstractAuditableMutableDomainObject implements Serializable {
 
     RuleSetBean ruleSetBean;
