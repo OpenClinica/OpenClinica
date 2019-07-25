e49eb3503c1c2597e61286664836573192ae018f
theywa <theywaquarius@gmail.com>
2019-07-25 09:44:12 +0800

    OC-11209 Imaging Upload Page in Runtime(Backend init)

---
+++ web/src/main/java/org/akaza/openclinica/controller/DicomController.java
@@ -0,0 +1,44 @@
+package org.akaza.openclinica.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping(value = "/auth/api/dicom")
+public class DicomController {
+
+    @RequestMapping(value = "/participantID/{participantID}/accessionID/{accessionID}/upload", method = RequestMethod.POST)
+    public ResponseEntity<Object> importDataPipeDelimitedFile(HttpServletRequest request,
+                                                              @PathVariable( "participantID" ) String participantID,
+                                                              @PathVariable( "accessionID" ) String accessionID,
+                                                              @RequestParam( "file" ) MultipartFile file) throws Exception {
+
+        if (file.isEmpty()) {
+            return ResponseEntity.ok("MISSING FILE");
+        } else {
+            try {
+                String filename = file.getOriginalFilename();
+                System.out.println("@@@@@@@@@ Upload  "+ filename);
+                long filesize = file.getSize();
+                double kilobytes = (filesize / 1024);
+                double megabytes = (kilobytes / 1024);
+                System.out.println("@@@@@@@@@ Size  : "+ megabytes + " MB");
+            } catch (Exception e) {
+                System.out.println("@@@@@@@@@ Error Upload : "+ e.getMessage());
+            }
+        }
+
+        if (participantID.equalsIgnoreCase("1")) {
+            return ResponseEntity.ok("UPLOAD SUCCESS");
+        } else {
+            return ResponseEntity.ok("UPLOAD FAILED");
+        }
+    }
+}
---
+++ web/src/main/webapp/WEB-INF/jsp/managestudy/dicomUploader.jsp
@@ -0,0 +1,42 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+
+<fmt:setBundle basename="org.akaza.openclinica.i18n.format" var="resformat"/>
+<fmt:setBundle basename="org.akaza.openclinica.i18n.notes" var="restext"/>
+<fmt:setBundle basename="org.akaza.openclinica.i18n.words" var="resword"/>
+<fmt:setBundle basename="org.akaza.openclinica.i18n.workflow" var="resworkflow"/>
+<c:set var="dteFormat"><fmt:message key="date_format_string" bundle="${resformat}"/></c:set>
+
+<jsp:include page="../include/managestudy-header.jsp"/>
+
+
+<!-- move the alert message to the sidebar-->
+<jsp:include page="../include/sideAlert.jsp"/>
+
+<!-- then instructions-->
+<tr id="sidebar_Instructions_open" style="display: none">
+		<td class="sidebar_tab">
+
+		<a href="javascript:leftnavExpand('sidebar_Instructions_open'); leftnavExpand('sidebar_Instructions_closed');"><span class="icon icon-caret-down gray"></span></a>
+
+		<fmt:message key="instructions" bundle="${restext}"/>
+
+		<div class="sidebar_tab_content">
+
+		</div>
+
+		</td>
+
+	</tr>
+	<tr id="sidebar_Instructions_closed" style="display: all">
+		<td class="sidebar_tab">
+
+		<a href="javascript:leftnavExpand('sidebar_Instructions_open'); leftnavExpand('sidebar_Instructions_closed');"><span class="icon icon-caret-right gray"></span></a>
+
+		<fmt:message key="instructions" bundle="${restext}"/>
+
+		</td>
+  </tr>
+<jsp:include page="../include/sideInfo.jsp"/>
+
