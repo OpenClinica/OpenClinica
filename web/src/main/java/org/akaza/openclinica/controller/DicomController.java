fa2cf7999a737f25a16b370acdd3e82025bdb069
theywa <theywaquarius@gmail.com>
2019-07-31 19:35:11 +0800

    OC-11209 Imaging Upload Page in Runtime (update upload API)

--- web/src/main/java/org/akaza/openclinica/controller/DicomController.java
+++ web/src/main/java/org/akaza/openclinica/controller/DicomController.java
@@ -1,5 +1,11 @@
 package org.akaza.openclinica.controller;
 
+import org.akaza.openclinica.bean.login.UserAccountBean;
+import org.akaza.openclinica.service.UtilService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,13 +20,24 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping(value = "/auth/api/dicom")
 public class DicomController {
 
+    @Autowired
+    private UtilService utilService;
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
+
     @RequestMapping(value = "/participantID/{participantID}/accessionID/{accessionID}/upload", method = RequestMethod.POST)
     public ResponseEntity<Object> importDataPipeDelimitedFile(HttpServletRequest request,
                                                               @PathVariable( "participantID" ) String participantID,
                                                               @PathVariable( "accessionID" ) String accessionID,
                                                               @RequestParam( "file" ) MultipartFile file) throws Exception {
 
+        UserAccountBean ownerUserAccountBean = utilService.getUserAccountFromRequest(request);
+        if (ownerUserAccountBean != null && ownerUserAccountBean.getId() < 1) {
+            return  new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
+
         if (file.isEmpty()) {
+            System.out.println("@@@@@@@@@ MISSING FILE @@@@@@@@@");
             return ResponseEntity.ok("MISSING FILE");
         } else {
             try {
@@ -36,9 +53,14 @@ public class DicomController {
         }
 
         if (participantID.equalsIgnoreCase("1")) {
+            System.out.println("@@@@@@@@@ UPLOAD SUCCESS @@@@@@@@@");
             return ResponseEntity.ok("UPLOAD SUCCESS");
-        } else {
+        } else if (participantID.equalsIgnoreCase("2")) {
+            System.out.println("@@@@@@@@@ UPLOAD FAILED @@@@@@@@@");
             return ResponseEntity.ok("UPLOAD FAILED");
+        } else {
+            System.out.println("@@@@@@@@@ UPLOAD PARTIAL @@@@@@@@@");
+            return ResponseEntity.ok("UPLOAD PARTIAL");
         }
     }
 }
