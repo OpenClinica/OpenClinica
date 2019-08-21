6003f004ade6e4446a0abff6053445299024ac40
Shiva Vadla <svadla@openclinica.com>
2019-08-21 17:10:35 -0400

    OC-11274: Removed unused imports

--- core/src/main/java/org/akaza/openclinica/service/user/CreateUserCoreService.java
+++ core/src/main/java/org/akaza/openclinica/service/user/CreateUserCoreService.java
@@ -3,7 +3,6 @@ package org.akaza.openclinica.service.user;
 import org.akaza.openclinica.bean.login.UserAccountBean;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
 import java.util.Map;
 
 public interface CreateUserCoreService {
