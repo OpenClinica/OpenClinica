a2aa3dd8ee1ef13b0389a620fbcb933d97aef8d0
Suthirr Balasubramanian <sbalasubramanian@openclinica.com>
2019-09-17 12:34:58 -0400

    OC-11459: Removed unwanted imports

--- web/src/main/java/org/akaza/openclinica/controller/StudyParticipantController.java
+++ web/src/main/java/org/akaza/openclinica/controller/StudyParticipantController.java
@@ -38,7 +38,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
