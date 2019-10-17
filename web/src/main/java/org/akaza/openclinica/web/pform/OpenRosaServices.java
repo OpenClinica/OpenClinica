cf95e92f346b99bfeded477c6744f7eded117182
Taoli2018 <35311591+Taoli2018@users.noreply.github.com>
2019-10-17 16:35:35 -0400

    OC-11143:Improve performance of XForm call when opening a form in Enketo (#2415)
    * OC-11143:Improve performance of XForm call when opening a form in Enketo
    
    * code change after code review
    
    * more code change  after code review

--- web/src/main/java/org/akaza/openclinica/web/pform/OpenRosaServices.java
+++ web/src/main/java/org/akaza/openclinica/web/pform/OpenRosaServices.java
@@ -444,8 +444,9 @@ public class OpenRosaServices {
             userList.setDownloadUrl(urlBase + "/rest2/openrosa/" + studyOID + "/downloadUsers?ecid=" + ecid);
             manifest.add(userList);
         }
-        MediaFile odmPayload = new MediaFile();
-        String odm = getODMMetadata(request, studyOID, ecid, context, formID);
+        
+        MediaFile odmPayload = new MediaFile();        
+        String odm = getODMMetadata(request, studyOID, ecid, context, formID,false);
         odmPayload.setHash((DigestUtils.md5Hex(odm)));
         odmPayload.setFilename("clinicaldata.xml");
         odmPayload.setDownloadUrl(urlBase + "/rest2/openrosa/" + studyOID + "/" + ecid + "/" + formID);
@@ -1080,7 +1081,7 @@ public class OpenRosaServices {
     @Path("/{studyOID}/{ecid}/{formID}")
     @Produces(MediaType.TEXT_XML)
     public String getODMMetadata(@Context HttpServletRequest request, @PathParam("studyOID") String studyOID, @PathParam("ecid") String ecid,
-            @Context ServletContext context, @PathParam("formID") String formID) throws Exception {
+            @Context ServletContext context, @PathParam("formID") String formID,@DefaultValue("true") @QueryParam(value="includeClinicalData") boolean includeClinicalData) throws Exception {
         if (!mayProceedPreview(request, studyOID))
             return null;
         HashMap<String, String> subjectContext = null;
@@ -1112,7 +1113,16 @@ public class OpenRosaServices {
         StudyEventDefinition sed = studyEventDefinitionDao.findById(Integer.valueOf(studyEventDefinitionID));
         String phraseToLookForInOdm = "<StudyEventData StudyEventOID=\"" + sed.getOc_oid() + "\" StudyEventRepeatKey=\"" + studyEventRepeat + "\"";
         String userAccountID = subjectContext.get("userAccountID");
-        String result = odmClinicalDataRestResource.getODMMetadata(studyOID, "*", studySubjectOID, "*", "no", "no", request, userAccountID, "yes","no", "yes","no","yes", "yes");
+        
+        String result = null;        
+        // first time call
+        if(!includeClinicalData) {
+        	result = odmClinicalDataRestResource.getODMMetadata(studyOID, "*", studySubjectOID, "*", "no", "no", request, userAccountID, "no","no", "no","no","yes", "yes");        	        
+        }else {
+        	// 2nd time call
+        	result = odmClinicalDataRestResource.getODMMetadata(studyOID, "*", studySubjectOID, "*", "no", "no", request, userAccountID, "yes","no", "yes","no","yes", "yes");        	
+        }
+        
         result = result.replaceAll("xmlns=\"http://www.cdisc.org/ns/odm/v1.3\"", "");
         result = result.replaceAll("xmlns:OpenClinica=\"http://www.openclinica.org/ns/odm_ext_v130/v3.1\"", "xmlns:OpenClinica=\"http://openclinica.com/odm\"");
         int index = result.indexOf(phraseToLookForInOdm);
