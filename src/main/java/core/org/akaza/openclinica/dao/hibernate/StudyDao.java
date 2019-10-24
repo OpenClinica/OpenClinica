1531071bbee595d6d155a4a2e4a0107555002fc5
Suthirr Balasubramanian <sbalasubramanian@openclinica.com>
2019-10-24 13:04:13 -0400

    OC-11624: StudyDAO equivalent Hibernate methods are created

--- src/main/java/core/org/akaza/openclinica/dao/hibernate/StudyDao.java
+++ src/main/java/core/org/akaza/openclinica/dao/hibernate/StudyDao.java
@@ -1,6 +1,10 @@
 package core.org.akaza.openclinica.dao.hibernate;
 
+import core.org.akaza.openclinica.bean.oid.OidGenerator;
+import core.org.akaza.openclinica.bean.oid.StudyOidGenerator;
 import core.org.akaza.openclinica.dao.core.CoreResources;
+import core.org.akaza.openclinica.dao.core.DAODigester;
+import core.org.akaza.openclinica.dao.core.SQLFactory;
 import core.org.akaza.openclinica.domain.datamap.Study;
 import core.org.akaza.openclinica.domain.datamap.StudyEnvEnum;
 import org.apache.commons.lang.StringUtils;
@@ -8,16 +12,59 @@ import org.hibernate.query.Query;
 import org.hibernate.transform.Transformers;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigInteger;
+import java.util.Collection;
 import java.util.List;
 
 @Transactional
 public class StudyDao extends AbstractDomainDao<Study> {
 
+    private DAODigester digester;
+
+    public StudyDao(){
+        String digesterName=SQLFactory.getInstance().DAO_STUDY;
+        digester= SQLFactory.getInstance().getDigester(digesterName);
+    }
     @Override
     public Class<Study> domainClass() {
         return Study.class;
     }
+
+    public Study update(Study study){
+        study.setOc_oid(getValidOid(study));
+        return saveOrUpdate(study);
+    }
+
+    public Study create(Study study){
+        study.setOc_oid(getValidOid(study));
+        save(study);
+        return study;
+    }
+    private String getOid(Study study) {
+        OidGenerator oidGenerator = new StudyOidGenerator();
+        String oid;
+        try {
+            oid = study.getOc_oid() != null ? study.getOc_oid() : oidGenerator.generateOid(study.getUniqueIdentifier());
+            return oid;
+        } catch (Exception e) {
+            throw new RuntimeException("CANNOT GENERATE OID");
+        }
+    }
+
+    public String getValidOid(Study study) {
+        OidGenerator oidGenerator = new StudyOidGenerator();
+        String oid = getOid(study);
+        String oidPreRandomization = oid;
+        while (findByOcOID(oid) != null) {
+            oid = oidGenerator.randomizeOid(oidPreRandomization);
+        }
+        return oid;
+    }
+
 
     public boolean doesStudyExist(String uniqueId, String schemaName) {
         Query q = getCurrentSession().createNativeQuery("select count(*) from " + schemaName + ".study where unique_identifier='" + uniqueId + "'");
@@ -30,6 +77,14 @@ public class StudyDao extends AbstractDomainDao<Study> {
         String query = " from Study do  where do.uniqueIdentifier = :uniqueId";
         Query q = getCurrentSession().createQuery(query);
         q.setParameter("uniqueId", uniqueId);
+        return (Study) q.uniqueResult();
+    }
+
+    public Study findSiteByUniqueIdentifier(String parentUniqueIdentifier, String siteUniqueIdentifier) {
+        String query=digester.getQuery("findSiteByUniqueIdentifier");
+        Query q=getCurrentSession().createNativeQuery(query);
+        q.setParameter("parentUniqueIdentifier",parentUniqueIdentifier);
+        q.setParameter("unique_identifier",siteUniqueIdentifier);
         return (Study) q.uniqueResult();
     }
 
@@ -114,6 +169,32 @@ public class StudyDao extends AbstractDomainDao<Study> {
         Study study = (Study) q.uniqueResult();
         CoreResources.setRequestSchema(schema);
         return study;
+    }
+
+    public Study findSiteByOid(String parentOid, String siteOid) {
+        String query="Select site.* from "+ getDomainClassName() +" site, "+getDomainClassName()+" study where site.study.studyId = study.studyId and" +
+                " study.oc_oid = :parentOid and site.oc_oid = :siteOid";
+        Query q=getCurrentSession().createQuery(query);
+        q.setParameter("parentOid",parentOid);
+        q.setParameter("siteOid",siteOid);
+        return (Study) q.uniqueResult();
+    }
+
+    public Study findStudyByOid(String studyOid) {
+        CriteriaBuilder cb = getCurrentSession().getCriteriaBuilder();
+        CriteriaQuery<Study> cq = cb.createQuery(Study.class);
+        Root<Study> study =  cq.from(Study.class);
+        Predicate predicate = cb.and(study.get("study.studyId").isNull(),
+                                cb.equal(study.get("oc_oid"),studyOid));
+        cq.select(study).where(predicate);
+        return (Study) getCurrentSession().createQuery(cq).uniqueResult();
+    }
+
+    public Collection findAllByUser(String username) {
+
+        Query q=getCurrentSession().createNativeQuery(digester.getQuery("findAllByUser")).setResultTransformer(Transformers.aliasToBean(Study.class));
+        q.setParameter("userName",username);
+        return q.list();
     }
 
     public Study updatePublicStudy(Study study) {
