086f529c66bdc28d047fb0195dd9c5efe163641d
Ron Panduwana <panduwana@gmail.com>
2019-11-12 00:19:00 +0700

    OC-11512 Issue w/ Schedule Button on Participant Matrix (#2438)

--- src/main/java/org/akaza/openclinica/control/submit/ListStudySubjectTableFactory.java
+++ src/main/java/org/akaza/openclinica/control/submit/ListStudySubjectTableFactory.java
@@ -1652,7 +1652,7 @@ public class ListStudySubjectTableFactory extends AbstractTableFactory {
         builder.a().href(href1 + href2);
         builder.onmouseover(onmouseover);
         builder.onmouseout(onmouseout);
-        builder.onclick(onClick1 + onClick2);
+        builder.onclick(onmouseover + onClick1 + onClick2);
         builder.close();
 
     }
@@ -1672,8 +1672,8 @@ public class ListStudySubjectTableFactory extends AbstractTableFactory {
         String onClick2 = "LockObject('Lock_" + studySubjectLabel + "_" + sed.getId() + "_" + rowCount + "',event); ";
         String href = studyEvents.size() > 1 ? href1Repeating + href2 : href1 + href2;
         builder.a().href(href);
-        // builder.onmouseover(onmouseover);
-        // builder.onmouseout(onmouseout);
+        builder.onmouseover(onmouseover);
+        builder.onmouseout(onmouseout);
         builder.onclick(onmouseover + onClick1 + onClick2);
         builder.close();
 
