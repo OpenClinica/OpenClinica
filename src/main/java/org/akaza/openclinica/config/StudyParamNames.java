3451a445d7cd9c6f96c46cacf89d36a7b6c59a33
suthirr-openclinica <54639317+suthirr-openclinica@users.noreply.github.com>
2019-12-18 15:09:05 -0500

    OC-11920: Saving some of the default values in the db is remved and Some places creating redundant child StudyParameterValues is removed (#2546)

--- src/main/java/org/akaza/openclinica/config/StudyParamNames.java
+++ src/main/java/org/akaza/openclinica/config/StudyParamNames.java
@@ -1,5 +1,8 @@
 package org.akaza.openclinica.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StudyParamNames {
     public static final String PERSON_ID_SHOWN_ON_CRF = "personIdShownOnCRF";
     public static final String INTERVIEW_DATE_EDITABLE = "interviewDateEditable";
@@ -22,4 +25,32 @@ public class StudyParamNames {
     public static final String ENFORCE_ENROLLMENT_CAP = "enforceEnrollmentCap";
     public static final String PARTICIPANT_ID_TEMPLATE = "participantIdTemplate";
     public static final String CONTACTS_MODULE = "contactsModule";
+
+    private static final Map<String, String> defaultValuesMap = new HashMap<String, String>();
+    static {
+        defaultValuesMap.put(PERSON_ID_SHOWN_ON_CRF, "false");
+        defaultValuesMap.put(INTERVIEW_DATE_EDITABLE, "true");
+        defaultValuesMap.put(INTERVIEW_DATE_DEFAULT, "blank");
+        defaultValuesMap.put(INTERVIEWER_NAME_EDITABLE, "true");
+        defaultValuesMap.put(INTERVIEW_DATE_REQUIRED, "not_used");
+        defaultValuesMap.put(INTERVIEWER_NAME_DEFAULT, "blank");
+        defaultValuesMap.put(INTERVIEWER_NAME_REQUIRED, "not_used");
+        defaultValuesMap.put(SUBJECT_ID_PREFIX_SUFFIX, "true");
+        defaultValuesMap.put(GENDER_REQUIRED, "true");
+        defaultValuesMap.put(SUBJECT_ID_GENERATION, "manual");
+        defaultValuesMap.put(SUBJECT_PERSON_ID_REQUIRED, "required");
+        defaultValuesMap.put(DISCREPANCY_MANAGEMENT, "true");
+        defaultValuesMap.put(COLLECT_DOB, "1");
+        defaultValuesMap.put(EVENT_LOCATION_REQUIRED, "not_used");
+        defaultValuesMap.put(SECONDARY_LABEL_VIEWABLE, "false");
+        defaultValuesMap.put(ADMIN_FORCED_REASON_FOR_CHANGE, "true");
+        defaultValuesMap.put(PARTICIPANT_PORTAL, "disabled");
+        defaultValuesMap.put(RANDOMIZATION, "disabled");
+        defaultValuesMap.put(ENFORCE_ENROLLMENT_CAP, "false");
+        defaultValuesMap.put(PARTICIPANT_ID_TEMPLATE, "");
+    }
+
+    public static String getDefaultValues(String name){
+        return defaultValuesMap.getOrDefault(name, "");
+    }
 }
