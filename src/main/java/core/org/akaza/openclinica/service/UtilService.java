874dd21e782a96deca4af69e21de8525ca8175d4
suthirr-openclinica <54639317+suthirr-openclinica@users.noreply.github.com>
2019-12-19 16:13:43 -0500

    OC-11777: ExtractParticipantInfo and Participant api is altered as per the ticket (#2550)

--- src/main/java/core/org/akaza/openclinica/service/UtilService.java
+++ src/main/java/core/org/akaza/openclinica/service/UtilService.java
@@ -3,11 +3,13 @@
  */
 package core.org.akaza.openclinica.service;
 
+import core.org.akaza.openclinica.bean.login.StudyUserRoleBean;
 import core.org.akaza.openclinica.bean.login.UserAccountBean;
 import core.org.akaza.openclinica.domain.datamap.Study;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 
 /**
  * @author joekeremian
@@ -29,4 +31,5 @@ public interface UtilService {
 
     void checkFileFormat(MultipartFile file, String fileHeaderMappring);
 
+	boolean checkStudyLevelUser(ArrayList<StudyUserRoleBean> userRoles, String siteOid);
 	}
