f47b1a298af76b5aaf4af0add86839fddf47405c
joekeremian <jkeremian@openclinica.com>
2019-12-23 22:54:17 -0500

    OC-11871 Support Participant IDs using random numbers

--- src/main/java/org/akaza/openclinica/controller/helper/TemplateHelper.java
+++ src/main/java/org/akaza/openclinica/controller/helper/TemplateHelper.java
@@ -1,36 +1,20 @@
 package org.akaza.openclinica.controller.helper;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import freemarker.template.TemplateException;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 @JsonIgnoreProperties
 public class TemplateHelper {
 
-
-    public Integer random(int digit_number) {
-        switch (digit_number) {
-            case 1:
-                return new Random().nextInt(9);
-            case 2:
-                return new Random().nextInt(99);
-            case 3:
-                return new Random().nextInt(999);
-            case 4:
-                return new Random().nextInt(9999);
-            case 5:
-                return new Random().nextInt(99999);
-            case 6:
-                return new Random().nextInt(999999);
-            case 7:
-                return new Random().nextInt(9999999);
-            case 8:
-                return new Random().nextInt(99999999);
-            case 9:
-                return new Random().nextInt(999999999);
-            default:
-                return null;
+    public Integer random(int digit_number) throws TemplateException {
+        if (digit_number < 1 || digit_number > 9) {
+            throw new TemplateException("Random number " + digit_number + " is out of range", null);
         }
+        int max = Integer.parseInt(StringUtils.repeat("9", digit_number));
+        return ThreadLocalRandom.current().nextInt(0, max + 1);
     }
-
 }
