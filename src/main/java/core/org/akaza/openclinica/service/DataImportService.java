a13de27486978b61b1c74b5a77abb8eafa07ab01
Suthirr Balasubramanian <sbalasubramanian@openclinica.com>
2020-01-02 14:56:28 -0500

    OC-9354: Sdv db changes in progress

--- src/main/java/core/org/akaza/openclinica/service/DataImportService.java
+++ src/main/java/core/org/akaza/openclinica/service/DataImportService.java
@@ -42,6 +42,7 @@ import core.org.akaza.openclinica.logic.rulerunner.ImportDataRuleRunnerContainer
 import core.org.akaza.openclinica.service.rule.RuleSetServiceInterface;
 import core.org.akaza.openclinica.web.job.CrfBusinessLogicHelper;
 import core.org.akaza.openclinica.web.job.TriggerService;
+import org.akaza.openclinica.domain.enumsupport.SdvStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -336,9 +337,9 @@ public class DataImportService {
                         eventCrfInts.add(new Integer(eventCrfBean.getId()));
                     }
                 }
-                // Reset the SDV status if item data has been changed or added
-                if (eventCrfBean != null && resetSDV)
-                    eventCrfDao.setSDVStatus(false, userBean.getId(), eventCrfBean.getId());
+                // Alter the SDV status if item data has been changed or added
+                if (eventCrfBean != null && resetSDV  && eventCrfBean.getSdvStatus() == SdvStatus.VERIFIED)
+                    eventCrfDao.setSDVStatus(SdvStatus.CHANGED_AFTER_VERIFIED, userBean.getId(), eventCrfBean.getId());
             }
         }
         if (!discNotesGenerated) {
