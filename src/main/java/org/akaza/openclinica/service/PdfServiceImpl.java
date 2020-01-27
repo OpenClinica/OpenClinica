b8ef9864eb3e69b7264d79abc42852221a871977
Tao Li <tli@openclinica.com>
2020-01-27 16:14:29 -0500

    Revert "OC-12094:The header of PDF Casebook need to support UTF-8 characters"
    This reverts commit 0200233359d7caddcef00fe93112c329d6e6e873.

--- src/main/java/org/akaza/openclinica/service/PdfServiceImpl.java
+++ src/main/java/org/akaza/openclinica/service/PdfServiceImpl.java
@@ -23,7 +23,6 @@ import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
 import org.apache.pdfbox.pdmodel.font.PDFont;
-import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.util.Matrix;
 import org.slf4j.Logger;
@@ -134,8 +133,8 @@ public class PdfServiceImpl implements PdfService {
      */
     public int addHeaderOrFooter(PDDocument document, String headerMsg,String footerMsg, int page_counter) throws IOException {
 
-        String footerMessage = null;      
-        PDFont font = PDType0Font.load(document, this.getClass().getResourceAsStream("/org/akaza/openclinica/i18n/utf8/uming.ttf"), false);
+        String footerMessage = null;             
+        PDFont font = PDType1Font.TIMES_ROMAN;
         float fontSize = 10.0f;
 
         for( PDPage page : document.getPages() )
@@ -240,7 +239,14 @@ public class PdfServiceImpl implements PdfService {
 			}
 	    	
 	    }
-	    		
+	    
+		// not support UTF-8 at this time
+		try {
+			pdfHeader = new String(pdfHeader.getBytes("ISO-8859-1"), "ISO-8859-1");
+		} catch (UnsupportedEncodingException e) {
+			;
+		}
+
 		// dynamically calculate the header length
 		while(pdfHeader.length() > 160) {
 			pdfHeader = pdfHeader.replaceFirst("  ", "");
