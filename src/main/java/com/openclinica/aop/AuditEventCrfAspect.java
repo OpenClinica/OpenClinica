34675eacccbb6ace0fdc86f4d6e38a67eb509bdf
Jmac <jmcinerney@openclinica.com>
2020-05-11 23:15:00 -0400

    Fixes for duplicates in AuditEventCRF Aspect

--- src/main/java/com/openclinica/aop/AuditEventCrfAspect.java
+++ src/main/java/com/openclinica/aop/AuditEventCrfAspect.java
@@ -3,13 +3,13 @@ package com.openclinica.aop;
 import com.openclinica.kafka.KafkaService;
 import core.org.akaza.openclinica.bean.managestudy.StudyEventBean;
 import core.org.akaza.openclinica.bean.submit.EventCRFBean;
+import core.org.akaza.openclinica.dao.hibernate.EventCrfDao;
 import core.org.akaza.openclinica.domain.datamap.EventCrf;
 import core.org.akaza.openclinica.domain.datamap.StudyEvent;
+import org.akaza.openclinica.domain.enumsupport.EventCrfWorkflowStatusEnum;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -19,29 +19,40 @@ import org.springframework.stereotype.Component;
 public class AuditEventCrfAspect {
     protected final Logger log = LoggerFactory.getLogger(getClass().getName());
     private KafkaService kafkaService;
+    private EventCrfDao eventCrfDao;
 
-    public AuditEventCrfAspect(KafkaService kafkaService){
+    public AuditEventCrfAspect(KafkaService kafkaService, EventCrfDao eventCrfDao){
         this.kafkaService = kafkaService;
+        this.eventCrfDao = eventCrfDao;
     }
 
-    @AfterReturning("execution(* core.org.akaza.openclinica.dao.hibernate.EventCrfDao.saveOrUpdate(..))")
-    public void onEventCrfDaoSaveOrUpdate(JoinPoint joinPoint) {
-        log.info("AoP: onEventCrfDaoSaveOrUpdate triggered");
-        try {
-            kafkaService.sendFormAttributeChangeMessage((EventCrf) joinPoint.getArgs()[0]);
-        } catch (Exception e) {
-            log.error("Could not send kafka message triggered by EventCrfDao.saveOrUpdate: ", e);
+    @Around("execution(* core.org.akaza.openclinica.dao.hibernate.EventCrfDao.saveOrUpdate(..))")
+    public EventCrf beforeEventCrfDaoSaveOrUpdate(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        EventCrf eventCrf = (EventCrf) proceedingJoinPoint.getArgs()[0];
+        boolean eventCrfIsSame = eventCrfIsTheSame(eventCrf);
+
+        EventCrf afterEventCrf = (EventCrf) proceedingJoinPoint.proceed();
+
+        if (!eventCrfIsSame){
+            log.info("AoP: triggered sendFormAttributeChangeMessage");
+            kafkaService.sendFormAttributeChangeMessage(afterEventCrf);
         }
+
+        return afterEventCrf;
     }
 
-    @AfterReturning("execution(* core.org.akaza.openclinica.dao.hibernate.EventCrfDao.save(..))")
-    public void onEventCrfDaoSave(JoinPoint joinPoint) {
-        log.info("AoP: onEventCrfDaoSave triggered");
-        try {
-            kafkaService.sendFormAttributeChangeMessage((EventCrf) joinPoint.getArgs()[0]);
-        } catch (Exception e) {
-            log.error("Could not send kafka message triggered by EventCrfDao.save: ", e);
+    public boolean eventCrfIsTheSame(EventCrf eventCrf){
+        EventCrf existingEventCrf = eventCrfDao.findById(eventCrf.getEventCrfId());
+        if (existingEventCrf != null){
+            if (existingEventCrf.getWorkflowStatus().equals(eventCrf.getWorkflowStatus())) {
+                log.info("AoP: Workflow status does match!");
+                return true; }
+            else {
+                log.info("AoP: Workflow status does NOT match!");
+                return false; }
         }
+        log.info("AoP: CRF must not exist, so it has changed.");
+        return false;
     }
 
     @AfterReturning("execution(* core.org.akaza.openclinica.dao.submit.EventCRFDAO.update(..))")
