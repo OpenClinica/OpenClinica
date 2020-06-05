b860bd08c101a373f31d588d845a2e1dbfdda4a4
Ron Panduwana <panduwana@gmail.com>
2020-06-05 20:42:04 +0700

    OC-12881 Update Participant Matrix non-repeating event popup to include event start date

--- src/main/java/org/akaza/openclinica/control/submit/ListStudySubjectTableFactory.java
+++ src/main/java/org/akaza/openclinica/control/submit/ListStudySubjectTableFactory.java
@@ -1485,10 +1485,10 @@ public class ListStudySubjectTableFactory extends AbstractTableFactory {
         eventDiv.append(eventText).append(": ").append(sed.getName()).br();
 
         if (!sed.isRepeating()) {
-            eventDiv.br().bold().append(eventStatus.getDisplayValue()).br();
             if (studyEvents.size() > 0 && studyEvents.get(0).getDateStarted() != null) {
                 eventDiv.append(formatDate(studyEvents.get(0).getDateStarted())).br();
             }
+            eventDiv.br().bold().append(eventStatus.getDisplayValue()).br();
             eventDiv.tdEnd();
             eventDiv.td(0).styleClass(tableHeaderRowLeftStyleClass).align("right").close();
             linkBuilder(eventDiv, studySubjectLabel, rowCount, studyEvents, sed);
