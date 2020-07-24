daea47895314711659763df00d4723bb7d2d5f29
Suthirr Balasubramanian <sbalasubramanian@openclinica.com>
2020-07-24 13:24:38 -0400

    OC-13088: Removed the Null condition to get all the latest values from userRoles

--- src/main/java/org/akaza/openclinica/control/login/ChangeStudyServlet.java
+++ src/main/java/org/akaza/openclinica/control/login/ChangeStudyServlet.java
@@ -162,12 +162,8 @@ public class ChangeStudyServlet extends SecureController {
 
     protected void populateCustomUserRoles(CustomRole customRole, String username) {
         List<ChangeStudyDTO> byUser = getStudyDao().findByUser(username);
-        List<StudyEnvironmentRoleDTO> userRoles = (List<StudyEnvironmentRoleDTO>) session.getAttribute("allUserRoles");
-        if (userRoles == null) {
-            logger.error("******************userRoles should not be null");
-            ResponseEntity<List<StudyEnvironmentRoleDTO>> responseEntity = getStudyBuildService().getUserRoles(request, true);
-            userRoles = responseEntity.getBody();
-        }
+        ResponseEntity<List<StudyEnvironmentRoleDTO>> responseEntity = getStudyBuildService().getUserRoles(request, true);
+        List<StudyEnvironmentRoleDTO>  userRoles = responseEntity.getBody();
         if (byUser == null) {
             logger.error("byUser variable should not be null for username:" + username);
         }
