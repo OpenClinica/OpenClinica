ec95a80fe1f206cb79d75f952250173dd4be4e6b
Tao Li <tli@openclinica.com>
2021-05-05 17:16:33 -0400

    OC-14979: add logic to load matched item metadata

--- ws/src/main/java/org/akaza/openclinica/web/crfdata/ImportCRFDataService.java
+++ ws/src/main/java/org/akaza/openclinica/web/crfdata/ImportCRFDataService.java
@@ -420,7 +420,25 @@ public class ImportCRFDataService {
                                         blankCheck.put(newKey, itemDataBean);
                                         logger.info("adding " + newKey + " to blank checks");
                                         if (!metadataBeans.isEmpty()) {
-                                            ItemFormMetadataBean metadataBean = metadataBeans.get(0);
+                                        	ItemFormMetadataBean metadataBean = null;
+                                           //OC-14979
+                                        	for(int i=0; i<metadataBeans.size();i++ ) {
+                                           		ItemFormMetadataBean metadataBeanTemp = metadataBeans.get(i);
+                                           		
+                                           		if(metadataBeanTemp.getCrfVersionId() == crfVersion.getId()) {
+                                           			metadataBean = metadataBeanTemp;
+                                           			break;
+                                           		}
+                                           	}
+                                           	
+										if(metadataBean == null) {
+											MessageFormat mf = new MessageFormat("");
+											mf.applyPattern(respage.getString("no_metadata_could_be_found"));
+											Object[] arguments = { importItemDataBean.getItemOID() };
+											
+											throw new OpenClinicaException(mf.format(arguments), "");	 
+										}
+                                           	
                                             // also possible nullpointer
                                             displayItemBean.setData(itemDataBean);
                                             displayItemBean.setMetadata(metadataBean);
