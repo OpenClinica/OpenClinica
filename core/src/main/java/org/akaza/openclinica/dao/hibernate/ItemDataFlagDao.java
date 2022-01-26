b152cc63019230c9973965a98e4386ea5322c18f
Tao Li <tli@openclinica.com>
2022-01-26 10:11:11 -0500

    OC-17141: fix reported SQL injection issues

--- core/src/main/java/org/akaza/openclinica/dao/hibernate/ItemDataFlagDao.java
+++ core/src/main/java/org/akaza/openclinica/dao/hibernate/ItemDataFlagDao.java
@@ -18,20 +18,26 @@ public class ItemDataFlagDao extends AbstractDomainDao<ItemDataFlag> {
 
     
     public List<ItemDataFlag> findAllByEventCrfPath(int tag_id , String eventCrfPath ) {
-
+    	
         String query = " from " + getDomainClassName() + "  where "
-                + " tag_id= " + tag_id +  " and path LIKE '" + eventCrfPath +".%'"  ;
+                + " tag_id = :tag_id and path LIKE :eventCrfPath";
         
         org.hibernate.Query q = getCurrentSession().createQuery(query);
+        q.setInteger("tag_id", tag_id);
+        q.setString("eventCrfPath", eventCrfPath + ".%");
+        
         return (List<ItemDataFlag>) q.list();
     }
 
     public ItemDataFlag findByItemDataPath(int tag_id ,  String itemDataPath ) {
 
         String query = " from " + getDomainClassName() + "  where "
-                + " tag_id= " + tag_id  + " and path= '" + itemDataPath +"'"   ;
+                + " tag_id= :tag_id  and path= :itemDataPath ";
         
         org.hibernate.Query q = getCurrentSession().createQuery(query);
+        q.setInteger("tag_id", tag_id);
+        q.setString("itemDataPath", itemDataPath);
+        
         return (ItemDataFlag) q.uniqueResult();
     }
 
