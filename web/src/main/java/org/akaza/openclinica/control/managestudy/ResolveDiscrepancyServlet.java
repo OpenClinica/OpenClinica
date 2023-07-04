64a000ca69fe97852b885021a88dede4fcfb59d3
Rehman Ali <rali@openclinica.com>
2023-07-04 12:04:51 +0500

    OC-21287 Locked event status forms should open in non-editable mode from discrepancies page

--- web/src/main/java/org/akaza/openclinica/control/managestudy/ResolveDiscrepancyServlet.java
+++ web/src/main/java/org/akaza/openclinica/control/managestudy/ResolveDiscrepancyServlet.java
@@ -93,7 +93,8 @@ public class ResolveDiscrepancyServlet extends SecureController {
 
             if (currentRole.getRole().equals(Role.MONITOR) ||
                 Objects.equals(note.getEvent().getSubjectEventStatus(), SubjectEventStatus.STOPPED) ||
-                Objects.equals(note.getEvent().getSubjectEventStatus(), SubjectEventStatus.SKIPPED)
+                Objects.equals(note.getEvent().getSubjectEventStatus(), SubjectEventStatus.SKIPPED) ||
+                Objects.equals(note.getEvent().getSubjectEventStatus(), SubjectEventStatus.LOCKED)
             ) {
                 return Page.VIEW_SECTION_DATA_ENTRY_SERVLET;
                 // ViewSectionDataEntry?eventDefinitionCRFId=&ecId=1&tabId=1&studySubjectId=1
@@ -159,6 +160,7 @@ public class ResolveDiscrepancyServlet extends SecureController {
             if (currentRole.getRole().equals(Role.MONITOR) ||
                 Objects.equals(note.getEvent().getSubjectEventStatus(), SubjectEventStatus.STOPPED) ||
                 Objects.equals(note.getEvent().getSubjectEventStatus(), SubjectEventStatus.SKIPPED) ||
+                Objects.equals(note.getEvent().getSubjectEventStatus(), SubjectEventStatus.LOCKED) ||
                 !isCompleted) {
                 StudyEventDAO sedao = new StudyEventDAO(ds);
                 StudyEventBean seb = (StudyEventBean) sedao.findByPK(id);
